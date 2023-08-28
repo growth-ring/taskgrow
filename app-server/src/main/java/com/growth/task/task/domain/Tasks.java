f0c5a900b624e94af3a856866b8fb3c6790d51e1
leekyuwon <kyuwoon369@gmail.com>
2023-08-28 19:21:25 +0900

    user를 지연로딩으로 설정하라

--- app-server/src/main/java/com/growth/task/task/domain/Tasks.java
+++ app-server/src/main/java/com/growth/task/task/domain/Tasks.java
@@ -5,17 +5,21 @@ import com.growth.task.user.domain.Users;
 import jakarta.persistence.Column;
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 public class Tasks extends BaseTimeEntity {
@@ -24,7 +28,7 @@ public class Tasks extends BaseTimeEntity {
     @Column(name = "task_id")
     private long taskId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id",
             referencedColumnName = "user_id",
             foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT)
@@ -38,8 +42,5 @@ public class Tasks extends BaseTimeEntity {
         this.taskId = taskId;
         this.user = user;
         this.taskDate = taskDate;
-    }
-
-    protected Tasks() {
     }
 }
