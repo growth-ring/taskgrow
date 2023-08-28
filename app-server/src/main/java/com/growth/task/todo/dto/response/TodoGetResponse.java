9814b56d64b7c02d2abb88cc833c8af07b9bfc81
caoyu <choyumin01@gmail.com>
2023-08-28 16:17:54 +0900

    todo dto  를 작성하라

--- app-server/src/main/java/com/growth/task/todo/dto/response/TodoGetResponse.java
+++ app-server/src/main/java/com/growth/task/todo/dto/response/TodoGetResponse.java
@@ -1,2 +1,17 @@
-package com.growth.task.todo.dto.response;public class TodoGetResponse {
+package com.growth.task.todo.dto.response;
+
+import com.growth.task.todo.enums.Status;
+import lombok.Data;
+
+@Data
+public class TodoGetResponse {
+    private Long todoId;
+    private String todo;
+    private Status status;
+
+    public TodoGetResponse(Long todoId, String todo, Status status) {
+        this.todoId = todoId;
+        this.todo = todo;
+        this.status = status;
+    }
 }
