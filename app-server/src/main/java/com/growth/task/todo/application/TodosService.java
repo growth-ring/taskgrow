d0240c3668dde8a606e8f791acb99562f4e75c72
caoyu <choyumin01@gmail.com>
2023-08-28 16:18:46 +0900

    todo 서비스를 작성하라

--- app-server/src/main/java/com/growth/task/todo/application/TodosService.java
+++ app-server/src/main/java/com/growth/task/todo/application/TodosService.java
@@ -1,7 +1,27 @@
 package com.growth.task.todo.application;
 
+import com.growth.task.todo.domain.Todos;
+import com.growth.task.todo.domain.TodosRepository;
+import com.growth.task.todo.dto.response.TodoGetResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-public class TodoService {
+public class TodosService {
+
+    private TodosRepository todosRepository;
+
+    public TodosService(TodosRepository todosRepository) {
+        this.todosRepository = todosRepository;
+    }
+
+    public List<TodoGetResponse> getTodosByTaskId(Long taskId) {
+        List<Todos> todosEntities = todosRepository.findByTask_TaskId(taskId);
+
+        return todosEntities.stream()
+                .map(entity -> new TodoGetResponse(entity.getTodoId(), entity.getTodo(), entity.getStatus()))
+                .collect(Collectors.toList());
+    }
 }
