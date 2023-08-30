3b980261b639307586979d1973540c750e700db8
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-08-30 17:27:15 +0900

    레파지토리 패키지로 분리하라

--- app-server/src/main/java/com/growth/task/task/repository/TasksRepository.java
+++ app-server/src/main/java/com/growth/task/task/repository/TasksRepository.java
@@ -1,5 +1,6 @@
-package com.growth.task.task.domain;
+package com.growth.task.task.repository;
 
+import com.growth.task.task.domain.Tasks;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
