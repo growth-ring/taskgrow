fd20db2e6548aef568dfa43533b0544e90f73fd8
caoyu <choyumin01@gmail.com>
2023-08-31 23:27:01 +0900

    TodoService 테스트에 taskId 를 추가하라

--- app-server/src/test/java/com/growth/task/todo/application/TodosServiceTest.java
+++ app-server/src/test/java/com/growth/task/todo/application/TodosServiceTest.java
@@ -2,6 +2,7 @@ package com.growth.task.todo.application;
 
 import com.growth.task.pomodoro.domain.Pomodoros;
 import com.growth.task.pomodoro.domain.PomodorosRepository;
+import com.growth.task.task.domain.Tasks;
 import com.growth.task.todo.domain.Todos;
 import com.growth.task.todo.domain.TodosRepository;
 import com.growth.task.todo.dto.response.TodoGetResponse;
@@ -47,14 +48,20 @@ public class TodosServiceTest {
 
             @BeforeEach
             void setUp() {
+                Tasks tasks = Tasks.builder()
+                        .taskId(1L)
+                        .build();
+
                 Todos todo1 = Todos.builder()
                         .todoId(1L)
+                        .task(tasks)
                         .todo("디자인패턴의 아름다움 스터디")
                         .status(Status.READY)
                         .build();
 
                 Todos todo2 = Todos.builder()
                         .todoId(2L)
+                        .task(tasks)
                         .todo("프로젝트 진행하기")
                         .status(Status.PROGRESS)
                         .build();
@@ -84,16 +91,17 @@ public class TodosServiceTest {
             void It_shouldReturnTodoGetResponseList() {
                 Long taskId = 1L;
                 List<TodoGetResponse> result = todosService.getTodosByTaskId(1L);
-                verifyTodoGetResponseList(result);
+                verifyTodoGetResponseList(result, taskId);
             }
         }
     }
 
-    private void verifyTodoGetResponseList(List<TodoGetResponse> result) {
+    private void verifyTodoGetResponseList(List<TodoGetResponse> result, Long taskId) {
         assertThat(result.size()).isEqualTo(2);
 
         TodoGetResponse firstResponse = result.get(0);
         assertThat(firstResponse.getTodoId()).isEqualTo(1L);
+        assertThat(firstResponse.getTaskId()).isEqualTo(taskId);
         assertThat(firstResponse.getTodo()).isEqualTo("디자인패턴의 아름다움 스터디");
         assertThat(firstResponse.getStatus()).isEqualTo(Status.READY);
         assertThat(firstResponse.getPerformCount()).isEqualTo(0);
@@ -101,6 +109,7 @@ public class TodosServiceTest {
 
         TodoGetResponse secondResponse = result.get(1);
         assertThat(secondResponse.getTodoId()).isEqualTo(2L);
+        assertThat(secondResponse.getTaskId()).isEqualTo(taskId);
         assertThat(secondResponse.getTodo()).isEqualTo("프로젝트 진행하기");
         assertThat(secondResponse.getStatus()).isEqualTo(Status.PROGRESS);
         assertThat(secondResponse.getPerformCount()).isEqualTo(1);
