c7bf6a007a728dba8ffa11105624404b8d0f4296
caoyu <choyumin01@gmail.com>
2023-08-31 23:28:06 +0900

    TodoGet 통합 테스트에 taskId 를 추가하라

--- app-server/src/test/java/com/growth/task/todo/controller/TodoControllerIntegrationTest.java
+++ app-server/src/test/java/com/growth/task/todo/controller/TodoControllerIntegrationTest.java
@@ -30,19 +30,21 @@ class TodoControllerIntegrationTest {
 
     @Test
     public void getTodosTest() throws Exception {
-        TodoGetResponse todo1 = new TodoGetResponse(1L, "디자인패턴의 아름다움 스터디", Status.READY, 1, 2);
-        TodoGetResponse todo2 = new TodoGetResponse(2L, "프로젝트 진행하기", Status.PROGRESS, 1, 2);
+        TodoGetResponse todo1 = new TodoGetResponse(1L, 1L, "디자인패턴의 아름다움 스터디", Status.READY, 1, 2);
+        TodoGetResponse todo2 = new TodoGetResponse(2L, 1L, "프로젝트 진행하기", Status.PROGRESS, 1, 2);
         List<TodoGetResponse> todoList = Arrays.asList(todo1, todo2);
         when(todosService.getTodosByTaskId(1L)).thenReturn(todoList);
 
         mockMvc.perform(get("/api/v1/todos/").param("id", "1"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].todoId").value(1L))
+                .andExpect(jsonPath("$[0].taskId").value(1L))
                 .andExpect(jsonPath("$[0].todo").value("디자인패턴의 아름다움 스터디"))
                 .andExpect(jsonPath("$[0].status").value("READY"))
                 .andExpect(jsonPath("$[0].performCount").value(1))
                 .andExpect(jsonPath("$[0].planCount").value(2))
                 .andExpect(jsonPath("$[1].todoId").value(2L))
+                .andExpect(jsonPath("$[1].taskId").value(1L))
                 .andExpect(jsonPath("$[1].todo").value("프로젝트 진행하기"))
                 .andExpect(jsonPath("$[1].status").value("PROGRESS"))
                 .andExpect(jsonPath("$[1].performCount").value(1))
