4af1caf885059e50c6676f4fc3cc45d44bdaf4ca
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-08-31 17:17:43 +0900

    Validation 에러를 핸들링하라

--- app-server/src/main/java/com/growth/task/commons/advice/ControllerErrorAdvice.java
+++ app-server/src/main/java/com/growth/task/commons/advice/ControllerErrorAdvice.java
@@ -3,9 +3,19 @@ package com.growth.task.commons.advice;
 import com.growth.task.task.exception.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Slf4j
 @ControllerAdvice
@@ -22,6 +32,23 @@ public class ControllerErrorAdvice {
         log.error("UserNotFoundException", exception);
         return exception.getMessage();
     }
+
+    /**
+     * javax.validation.Valid or @Validated으로 binding error 발생 시 발생한다.
+     */
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        log.error("MethodArgumentNotValidException", exception);
+
+        BindingResult bindingResult = exception.getBindingResult();
+
+        Map<String, String> errors = new HashMap<>();
+
+        List<ObjectError> allErrors = bindingResult.getAllErrors();
+        for (ObjectError error : allErrors) {
+            errors.put(((FieldError) error).getField(), error.getDefaultMessage());
+        }
+        return errors;
+    }
 }
-
-
