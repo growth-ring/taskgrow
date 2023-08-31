e64a159b3729893340004cf5e1bb1e20a4e6bc5e
caoyu <choyumin01@gmail.com>
2023-08-31 20:57:01 +0900

    pomodoros 레파지토리를 추가하라

--- app-server/src/main/java/com/growth/task/pomodoro/domain/PomodorosRepository.java
+++ app-server/src/main/java/com/growth/task/pomodoro/domain/PomodorosRepository.java
@@ -3,6 +3,9 @@ package com.growth.task.pomodoro.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PomodorosRepository extends JpaRepository<Pomodoros, Long> {
+    Optional<Pomodoros> findByTodo_TodoId(Long todoId);
 }
