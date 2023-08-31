10e33181e2c5cb4599a3abc4bc7a9900c0125d86
caoyu <choyumin01@gmail.com>
2023-08-31 20:54:09 +0900

    TodoGetResponse 클래스에 pomodoros 도메인의 카운트 데이터를 추가하라

--- app-server/src/main/java/com/growth/task/todo/dto/response/TodoGetResponse.java
+++ app-server/src/main/java/com/growth/task/todo/dto/response/TodoGetResponse.java
@@ -1,5 +1,7 @@
 package com.growth.task.todo.dto.response;
 
+import com.growth.task.pomodoro.domain.Pomodoros;
+import com.growth.task.todo.domain.Todos;
 import com.growth.task.todo.enums.Status;
 import lombok.Data;
 
@@ -8,10 +10,22 @@ public class TodoGetResponse {
     private Long todoId;
     private String todo;
     private Status status;
+    private Integer performCount;
+    private Integer planCount;
 
-    public TodoGetResponse(Long todoId, String todo, Status status) {
+    public TodoGetResponse(Long todoId, String todo, Status status, Integer performCount, Integer planCount) {
         this.todoId = todoId;
         this.todo = todo;
         this.status = status;
+        this.performCount = performCount;
+        this.planCount = planCount;
+    }
+
+    public TodoGetResponse(Todos todos, Pomodoros pomodoros) {
+        this.todoId = todos.getTodoId();
+        this.todo = todos.getTodo();
+        this.status = todos.getStatus();
+        this.performCount = pomodoros.getPerformCount();
+        this.planCount = pomodoros.getPlanCount();
     }
 }
