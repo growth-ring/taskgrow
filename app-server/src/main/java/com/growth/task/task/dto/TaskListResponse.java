2d6d95e8930e19bd79b5416b3f581823a0b9ba67
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-09-01 10:30:14 +0900

    todos의 남은 개수, 종료 개수를 담은 객체로 수정하라

--- app-server/src/main/java/com/growth/task/task/dto/TaskListResponse.java
+++ app-server/src/main/java/com/growth/task/task/dto/TaskListResponse.java
@@ -3,14 +3,33 @@ package com.growth.task.task.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+/**
+ * Task 리스트 조회 DTO
+ */
+@NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Getter
 public class TaskListResponse {
     private Long taskId;
     private Long userId;
     private LocalDateTime taskDate;
-    private Long todos;
+    private TaskTodoResponse todos;
+
+    public TaskListResponse(Long taskId, Long userId, LocalDateTime taskDate, TaskTodoResponse todos) {
+        this.taskId = taskId;
+        this.userId = userId;
+        this.taskDate = taskDate;
+        this.todos = todos;
+    }
+
+    public TaskListResponse(TaskListWithTodoStatusResponse task, TaskTodoResponse todos) {
+        this.taskId = task.getTaskId();
+        this.userId = task.getUserId();
+        this.taskDate = task.getTaskDate();
+        this.todos = todos;
+    }
 }
--- app-server/src/main/java/com/growth/task/task/dto/TaskTodoResponse.java
+++ app-server/src/main/java/com/growth/task/task/dto/TaskTodoResponse.java
@@ -5,14 +5,17 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * Task의 Todos 진행률 DTO
+ */
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Getter
 public class TaskTodoResponse {
-    private Long remain;
-    private Long done;
+    private int remain;
+    private int done;
 
-    public TaskTodoResponse(Long remain, Long done) {
+    public TaskTodoResponse(int remain, int done) {
         this.remain = remain;
         this.done = done;
     }
