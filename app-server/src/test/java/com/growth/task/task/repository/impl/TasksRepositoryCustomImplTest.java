f4da002f56436283c27e8d5c9dbcc65b3e17a767
leekyuwon <kyuwoon369@gmail.com>
2023-09-02 01:18:18 +0900

    테스트 더미를 테스트 실행 전에 만들도록 수정하라

--- app-server/src/test/java/com/growth/task/task/repository/impl/TasksRepositoryCustomImplTest.java
+++ app-server/src/test/java/com/growth/task/task/repository/impl/TasksRepositoryCustomImplTest.java
@@ -10,6 +10,7 @@ import com.growth.task.todo.enums.Status;
 import com.growth.task.user.domain.Users;
 import com.growth.task.user.domain.UsersRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -53,8 +54,8 @@ class TasksRepositoryCustomImplTest {
         );
     }
 
-    private Todos getTodo(Tasks task, String todo, Status status) {
-        return todosRepository.save(
+    private void getTodo(Tasks task, String todo, Status status) {
+        todosRepository.save(
                 Todos.builder()
                         .task(task)
                         .todo(todo)
@@ -73,25 +74,38 @@ class TasksRepositoryCustomImplTest {
     @Nested
     @DisplayName("findRemainedTodosByUserBetweenTimeRange")
     class Describe_findRemainedTodosByUserBetweenTimeRange {
-        Users user = getUser("test1", "password");
-        Tasks task0 = getTask(user, LocalDate.parse("2023-08-28"));
-        Tasks task1 = getTask(user, LocalDate.parse("2023-08-29"));
-        Tasks task2 = getTask(user, LocalDate.parse("2023-08-30"));
-        Tasks task3 = getTask(user, LocalDate.parse("2023-08-31"));
-        Todos todoTask1StatusReady = getTodo(task1, "디자인 패턴의 아름다움 읽기", Status.READY);
-        Todos todoTask1StatusReady2 = getTodo(task1, "얼고리즘 읽기", Status.READY);
-        Todos todoTask1StatusDone1 = getTodo(task1, "스프링 인 액션 읽기", Status.DONE);
-        Todos todoTask1StatusDone2 = getTodo(task1, "파이브 라인스 오브 코드 읽기", Status.DONE);
-        Todos todoTask1StatusProgress = getTodo(task1, "구글 엔지니어는 이렇게 일한다 읽기", Status.PROGRESS);
-        Todos todoTask2StatusReady = getTodo(task2, "견고한 데이터 엔지니어링 읽기", Status.PROGRESS);
-        Todos todoTask2StatusReady2 = getTodo(task2, "레거시 코드 활용 전략 읽기", Status.READY);
-        Todos todoTask2StatusDone = getTodo(task2, "파이썬 코딩의 기술 읽기", Status.DONE);
-        Todos todoTask2StatusDone2 = getTodo(task2, "Two Scoops of Django 읽기", Status.DONE);
-        Todos todoTask3StatusReady = getTodo(task3, "운동하기", Status.DONE);
+        private Users user;
+        private Tasks task0;
+        private Tasks task1;
+        private Tasks task2;
+        private Tasks task3;
+
+        @BeforeEach
+        void set() {
+            user = getUser("test1", "password");
+            task0 = getTask(user, LocalDate.parse("2023-08-28"));
+            task1 = getTask(user, LocalDate.parse("2023-08-29"));
+            task2 = getTask(user, LocalDate.parse("2023-08-30"));
+            task3 = getTask(user, LocalDate.parse("2023-08-31"));
+        }
 
         @Nested
         @DisplayName("user id와 task_date 날짜 범위가 주어지면")
         class Context_with_user_id_and_task_date_range {
+            @BeforeEach
+            void setContext() {
+                getTodo(task1, "디자인 패턴의 아름다움 읽기", Status.READY);
+                getTodo(task1, "얼고리즘 읽기", Status.READY);
+                getTodo(task1, "스프링 인 액션 읽기", Status.DONE);
+                getTodo(task1, "파이브 라인스 오브 코드 읽기", Status.DONE);
+                getTodo(task1, "구글 엔지니어는 이렇게 일한다 읽기", Status.PROGRESS);
+                getTodo(task2, "견고한 데이터 엔지니어링 읽기", Status.PROGRESS);
+                getTodo(task2, "레거시 코드 활용 전략 읽기", Status.READY);
+                getTodo(task2, "파이썬 코딩의 기술 읽기", Status.DONE);
+                getTodo(task2, "Two Scoops of Django 읽기", Status.DONE);
+                getTodo(task3, "운동하기", Status.DONE);
+            }
+
             @Test
             @DisplayName("task 정보와 todo status를 반환한다")
             void it_return_task_info_and_todo_count() {
