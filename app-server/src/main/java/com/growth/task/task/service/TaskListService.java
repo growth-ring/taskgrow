e3e6bcfab3586fce7dfabc84af8cbea905ebd9f0
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-09-01 10:33:42 +0900

    todo 진행률을 담은 Task 리스트를 조회하라

--- app-server/src/main/java/com/growth/task/task/service/TaskListService.java
+++ app-server/src/main/java/com/growth/task/task/service/TaskListService.java
@@ -1,9 +1,95 @@
 package com.growth.task.task.service;
 
+import com.growth.task.task.dto.TaskListRequest;
+import com.growth.task.task.dto.TaskListResponse;
+import com.growth.task.task.dto.TaskListWithTodoStatusResponse;
+import com.growth.task.task.dto.TaskTodoResponse;
 import com.growth.task.task.repository.TasksRepository;
+import com.growth.task.todo.enums.Status;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.growth.task.todo.enums.Status.isDone;
+import static com.growth.task.todo.enums.Status.isRemain;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 @Service
+@Transactional
 public class TaskListService {
-    private TasksRepository tasksRepository;
+    private final TasksRepository tasksRepository;
+
+    public TaskListService(TasksRepository tasksRepository) {
+        this.tasksRepository = tasksRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<TaskListResponse> getTasks(TaskListRequest request) {
+        List<TaskListWithTodoStatusResponse> taskList = tasksRepository.findRemainedTodosByUserBetweenTimeRange(
+                request.getUserId(),
+                request.getStartDate().atStartOfDay(),
+                request.getEndDate().atStartOfDay()
+        );
+
+        Map<Long, TaskTodoResponse> groupingByTask = groupingByTask(taskList);
+
+        return taskList.stream()
+                .map(task -> {
+                            TaskTodoResponse todos = groupingByTask.getOrDefault(task.getTaskId(), new TaskTodoResponse(0, 0));
+                            return new TaskListResponse(task, todos);
+                        }
+                )
+                .toList();
+    }
+
+    /**
+     * Task의 id를 기준으로 todos의 status의 개수를 세어 그룹화합니다.
+     * 예를 들어,
+     * [ {taskId: 1, userId: 1, taskDate: 2023-08-28, todoStatus: READY},
+     * {taskId: 1, userId: 1, taskDate: 2023-08-28, todoStatus: READY},
+     * {taskId: 1, userId: 1, taskDate: 2023-08-28, todoStatus: READY} ]
+     * 의 경우
+     * {taskId: 1, {remain: 3, done: 0}}
+     * 을 반환합니다.
+     *
+     * @param taskList Task 리스트
+     * @return 테스크의 진행률 Map
+     */
+    public static Map<Long, TaskTodoResponse> groupingByTask(List<TaskListWithTodoStatusResponse> taskList) {
+        return taskList.stream()
+                .collect(groupingBy(
+                                TaskListWithTodoStatusResponse::getTaskId,
+                                collectingAndThen(toList(),
+                                        TaskListService::calculateTodoStatus
+                                )
+                        )
+                );
+    }
+
+    /**
+     * Todos 진행률을 계산합니다.
+     * remain : 남은 Todos 개수 (status= READY, PROGRESS)
+     * done : 끝난 Todos 개수 (statsu= DONE)
+     * ex) {'remain': 3, 'done' : 2}
+     *
+     * @param tasks Task 리스트
+     * @return Todos 진행률
+     */
+    public static TaskTodoResponse calculateTodoStatus(List<TaskListWithTodoStatusResponse> tasks) {
+        int doneCount = 0;
+        int remainCount = 0;
+        for (TaskListWithTodoStatusResponse task : tasks) {
+            Status todoStatus = task.getTodoStatus();
+            if (isDone(todoStatus)) {
+                doneCount++;
+            } else if (isRemain(todoStatus)) {
+                remainCount++;
+            }
+        }
+        return new TaskTodoResponse(remainCount, doneCount);
+    }
 }
---
+++ app-server/src/test/java/com/growth/task/task/service/TaskListServiceTest.java
@@ -0,0 +1,259 @@
+package com.growth.task.task.service;
+
+import com.growth.task.task.domain.Tasks;
+import com.growth.task.task.dto.TaskListRequest;
+import com.growth.task.task.dto.TaskListResponse;
+import com.growth.task.task.dto.TaskListWithTodoStatusResponse;
+import com.growth.task.task.dto.TaskTodoResponse;
+import com.growth.task.task.repository.TasksRepository;
+import com.growth.task.todo.domain.Todos;
+import com.growth.task.todo.enums.Status;
+import com.growth.task.user.domain.Users;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import static com.growth.task.task.service.TaskListService.calculateTodoStatus;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("TaskListService")
+class TaskListServiceTest {
+    private TaskListService taskListService;
+    @Mock
+    private TasksRepository tasksRepository;
+
+    @BeforeEach
+    void setUp() {
+        taskListService = new TaskListService(tasksRepository);
+    }
+
+    @Nested
+    @DisplayName("getTasks")
+    class Describe_getTasks {
+        @Nested
+        @DisplayName("user Id와 start_date, end_date가 넘어오면")
+        class Context_with_user_id_and_start_date_and_end_date {
+            Users givenUser = Users.builder()
+                    .userId(1L)
+                    .name("user1")
+                    .password("password")
+                    .build();
+            Tasks givenTask = Tasks.builder()
+                    .taskId(1L)
+                    .taskDate(LocalDateTime.parse("2023-08-28T00:00:00"))
+                    .user(givenUser)
+                    .build();
+            Todos givenTodo1 = Todos.builder()
+                    .todoId(1L)
+                    .task(givenTask)
+                    .todo("api 작업")
+                    .status(Status.READY)
+                    .build();
+            Todos givenTodo2 = Todos.builder()
+                    .todoId(2L)
+                    .task(givenTask)
+                    .todo("과제하기")
+                    .status(Status.READY)
+                    .build();
+            Todos givenTodo3 = Todos.builder()
+                    .todoId(3L)
+                    .task(givenTask)
+                    .todo("오브젝트 읽기")
+                    .status(Status.READY)
+                    .build();
+            TaskListRequest request = TaskListRequest.builder()
+                    .userId(givenUser.getUserId())
+                    .startDate(LocalDate.parse("2023-08-01"))
+                    .endDate(LocalDate.parse("2023-08-30"))
+                    .build();
+
+            @BeforeEach
+            void prepare() {
+                given(tasksRepository.findRemainedTodosByUserBetweenTimeRange(
+                                request.getUserId(),
+                                request.getStartDate().atStartOfDay(),
+                                request.getEndDate().atStartOfDay()
+                        )
+                ).willReturn(List.of(
+                        new TaskListWithTodoStatusResponse(1L, 1L, givenTask.getTaskDate(), givenTodo1.getStatus()),
+                        new TaskListWithTodoStatusResponse(1L, 1L, givenTask.getTaskDate(), givenTodo2.getStatus()),
+                        new TaskListWithTodoStatusResponse(1L, 1L, givenTask.getTaskDate(), givenTodo3.getStatus())
+                ));
+            }
+
+            @Test
+            @DisplayName("task 리스트를 리턴한다")
+            void it_return_task_list() {
+                List<TaskListResponse> result = taskListService.getTasks(request);
+                assertAll(
+                        () -> assertThat(result).hasSize(3),
+                        () -> assertThat(result.get(0).getTodos())
+                );
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("groupingByTask")
+    class Describe_groupingByTask {
+
+        @Nested
+        @DisplayName("todo status를 가진 task 리스트가 주어지면")
+        class Context_with_task_list_with_todo_status {
+            List<TaskListWithTodoStatusResponse> taskList = List.of(
+                    new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.READY),
+                    new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.READY),
+                    new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.PROGRESS),
+                    new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.PROGRESS),
+                    new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.DONE),
+                    new TaskListWithTodoStatusResponse(2L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.READY),
+                    new TaskListWithTodoStatusResponse(2L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.READY),
+                    new TaskListWithTodoStatusResponse(2L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.READY),
+                    new TaskListWithTodoStatusResponse(3L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.DONE),
+                    new TaskListWithTodoStatusResponse(3L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.DONE),
+                    new TaskListWithTodoStatusResponse(3L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.DONE),
+                    new TaskListWithTodoStatusResponse(3L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.DONE),
+                    new TaskListWithTodoStatusResponse(3L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.DONE),
+                    new TaskListWithTodoStatusResponse(4L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), null)
+
+            );
+
+            @Test
+            @DisplayName("task id로 status 별 개수를 카운팅한 map을 리턴한다")
+            void it_return_map_grouping_by_task() {
+                Map<Long, TaskTodoResponse> result = taskListService.groupingByTask(taskList);
+                System.out.println(result);
+
+                assertAll(
+                        () -> assertThat(result).hasSize(4),
+                        () -> assertThat(result.get(1L).getRemain()).isEqualTo(4),
+                        () -> assertThat(result.get(1L).getDone()).isEqualTo(1),
+                        () -> assertThat(result.get(2L).getRemain()).isEqualTo(3),
+                        () -> assertThat(result.get(2L).getDone()).isEqualTo(0),
+                        () -> assertThat(result.get(3L).getRemain()).isEqualTo(0),
+                        () -> assertThat(result.get(3L).getDone()).isEqualTo(5),
+                        () -> assertThat(result.get(4L).getRemain()).isEqualTo(0),
+                        () -> assertThat(result.get(4L).getDone()).isEqualTo(0)
+                );
+
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("calculateTodoStatus")
+    class Describe_calculateTodoStatus {
+        @Nested
+        @DisplayName("task 리스트가 주어지면")
+        class Context_with_task_list {
+            @Nested
+            @DisplayName("READY, PROGRESS, DONE이 골고루 존재하면")
+            class Context_with_todo_status_ready_progress_done {
+                List<TaskListWithTodoStatusResponse> taskList = List.of(
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.READY),
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.READY),
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.PROGRESS),
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.PROGRESS),
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.DONE)
+                );
+
+                @Test
+                @DisplayName("remain 4, done 1이 나온다")
+                void it_return_remain_4_and_done_1() {
+                    TaskTodoResponse result = calculateTodoStatus(taskList);
+                    assertAll(
+                            () -> assertThat(result.getRemain()).isEqualTo(4),
+                            () -> assertThat(result.getDone()).isEqualTo(1)
+                    );
+                }
+            }
+
+            @Nested
+            @DisplayName("READY만 존재하면")
+            class Context_with_todo_status_is_only_ready {
+                List<TaskListWithTodoStatusResponse> taskList = List.of(
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.READY),
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.READY)
+                );
+
+                @Test
+                @DisplayName("remain 2, done 0이 나온다")
+                void it_return_remain_2_and_done_0() {
+                    TaskTodoResponse result = calculateTodoStatus(taskList);
+                    assertAll(
+                            () -> assertThat(result.getRemain()).isEqualTo(2),
+                            () -> assertThat(result.getDone()).isEqualTo(0)
+                    );
+                }
+            }
+
+            @Nested
+            @DisplayName("READY와 PROGRESS만 존재하면")
+            class Context_with_todo_status_is_ready_or_progress {
+                List<TaskListWithTodoStatusResponse> taskList = List.of(
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.READY),
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.PROGRESS)
+                );
+
+                @Test
+                @DisplayName("remain 2, done 0이 나온다")
+                void it_return_remain_2_and_done_0() {
+                    TaskTodoResponse result = calculateTodoStatus(taskList);
+                    assertAll(
+                            () -> assertThat(result.getRemain()).isEqualTo(2),
+                            () -> assertThat(result.getDone()).isEqualTo(0)
+                    );
+                }
+            }
+
+            @Nested
+            @DisplayName("DONE만 존재하면")
+            class Context_with_todo_status_is_only_done {
+                List<TaskListWithTodoStatusResponse> taskList = List.of(
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.DONE),
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), Status.DONE)
+                );
+
+                @Test
+                @DisplayName("remain 0, done 2이 나온다")
+                void it_return_remain_0_and_done_2() {
+                    TaskTodoResponse result = calculateTodoStatus(taskList);
+                    assertAll(
+                            () -> assertThat(result.getRemain()).isEqualTo(0),
+                            () -> assertThat(result.getDone()).isEqualTo(2)
+                    );
+                }
+            }
+
+            @Nested
+            @DisplayName("존재하지 않다면")
+            class Context_with_todo_status_is_null {
+                List<TaskListWithTodoStatusResponse> taskList = List.of(
+                        new TaskListWithTodoStatusResponse(1L, 1L, LocalDateTime.parse("2023-08-28T00:00:00"), null)
+                );
+
+                @Test
+                @DisplayName("remain 0, done 0이 나온다")
+                void it_return_remain_0_and_done_0() {
+                    TaskTodoResponse result = calculateTodoStatus(taskList);
+                    assertAll(
+                            () -> assertThat(result.getRemain()).isEqualTo(0),
+                            () -> assertThat(result.getDone()).isEqualTo(0)
+                    );
+                }
+            }
+        }
+    }
+}
