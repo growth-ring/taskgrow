ca2cb7ecf9aca097c581fdab7826a9cb82a17291
caoyu <choyumin01@gmail.com>
2023-09-01 14:18:38 +0900

    taskId 로 Tasks 가 확인되지 않는 경우 TaskNotFoundException 이 발생하고, 확인되는 경우 Todos 를 조회하여 값이 있는 경우 해당 값을 반환하고, 값이 없는 경우 빈 값을 반환하라

--- app-server/src/main/java/com/growth/task/todo/application/TodosService.java
+++ app-server/src/main/java/com/growth/task/todo/application/TodosService.java
@@ -2,12 +2,14 @@ package com.growth.task.todo.application;
 
 import com.growth.task.pomodoro.domain.Pomodoros;
 import com.growth.task.pomodoro.domain.PomodorosRepository;
+import com.growth.task.task.domain.TasksRepository;
 import com.growth.task.todo.domain.Todos;
 import com.growth.task.todo.domain.TodosRepository;
 import com.growth.task.todo.dto.response.TodoGetResponse;
 import com.growth.task.todo.exception.TaskNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,16 +18,24 @@ public class TodosService {
 
     private TodosRepository todosRepository;
     private PomodorosRepository pomodorosRepository;
+    private TasksRepository tasksRepository;
 
-    public TodosService(TodosRepository todosRepository, PomodorosRepository pomodorosRepository) {
+    public TodosService(
+            TodosRepository todosRepository, 
+            PomodorosRepository pomodorosRepository, 
+            TasksRepository tasksRepository
+    ) {
         this.todosRepository = todosRepository;
         this.pomodorosRepository = pomodorosRepository;
+        this.tasksRepository = tasksRepository;
     }
 
     public List<TodoGetResponse> getTodosByTaskId(Long taskId) {
-        List<Todos> todosEntities = todosRepository.findByTask_TaskId(taskId);
+        List<Todos> todosEntities = validateTaskAndFetchTodos(taskId);
+
+        // Todo 가 없으면 빈 리스트 반환
         if (todosEntities.isEmpty()) {
-            throw new TaskNotFoundException();
+            return Collections.emptyList();
         }
 
         return todosEntities.stream()
@@ -34,5 +44,12 @@ public class TodosService {
                     return new TodoGetResponse(todo, pomodoro);
                 })
                 .collect(Collectors.toList());
+    }
+
+    private List<Todos> validateTaskAndFetchTodos(Long taskId) {
+        if(!tasksRepository.existsById(taskId)) {
+            throw new TaskNotFoundException();
+        }
+        return todosRepository.findByTask_TaskId(taskId);
     }
 }
