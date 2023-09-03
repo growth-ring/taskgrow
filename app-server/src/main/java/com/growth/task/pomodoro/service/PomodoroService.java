c81e6bdf6e69f286f5615a12f7ac3b51e4871587
caoyu <choyumin01@gmail.com>
2023-09-03 17:47:11 +0900

    PomodoroService 클래스에 Pomodoros 를 저장할 save 메서드를 작성하라

--- app-server/src/main/java/com/growth/task/pomodoro/service/PomodoroService.java
+++ app-server/src/main/java/com/growth/task/pomodoro/service/PomodoroService.java
@@ -1,2 +1,21 @@
-package com.growth.task.pomodoro.service;public class PomodoroService {
+package com.growth.task.pomodoro.service;
+
+import com.growth.task.pomodoro.domain.Pomodoros;
+import com.growth.task.pomodoro.domain.PomodorosRepository;
+import com.growth.task.pomodoro.dto.request.PomodoroAddRequest;
+import com.growth.task.todo.domain.Todos;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PomodoroService {
+    private final PomodorosRepository pomodorosRepository;
+
+    public PomodoroService(PomodorosRepository pomodorosRepository) {
+        this.pomodorosRepository = pomodorosRepository;
+    }
+
+    public Pomodoros save(PomodoroAddRequest pomodoroAddRequest, Todos todos) {
+        Pomodoros pomodoros = pomodoroAddRequest.toEntity(todos);
+        return pomodorosRepository.save(pomodoros);
+    }
 }
