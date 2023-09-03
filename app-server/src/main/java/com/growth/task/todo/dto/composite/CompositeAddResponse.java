b2ba8781fc54ea16a9d8d4249e35cb1c2ffc0457
caoyu <choyumin01@gmail.com>
2023-09-03 17:49:54 +0900

    TodoAddResponse 와 PomodoroAddResponse 정보를 한 번에 저장하기 위한 응답 데이터를 작성하라

--- app-server/src/main/java/com/growth/task/todo/dto/composite/CompositeAddResponse.java
+++ app-server/src/main/java/com/growth/task/todo/dto/composite/CompositeAddResponse.java
@@ -1,4 +1,21 @@
 package com.growth.task.todo.dto.composite;
 
-public class CompositeAddRequest {
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.growth.task.pomodoro.dto.response.PomodoroAddResponse;
+import com.growth.task.todo.dto.response.TodoAddResponse;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CompositeAddResponse {
+    private TodoAddResponse todoAddResponse;
+    private PomodoroAddResponse pomodoroAddResponse;
+
+    @Builder
+    public CompositeAddResponse(TodoAddResponse todoAddResponse, PomodoroAddResponse pomodoroAddResponse) {
+        this.todoAddResponse = todoAddResponse;
+        this.pomodoroAddResponse = pomodoroAddResponse;
+    }
 }
