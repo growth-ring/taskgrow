c6df88af7415437500c60898a3e8d314268b75dc
caoyu <choyumin01@gmail.com>
2023-09-03 17:36:24 +0900

    PomodoroAddRequest 를 작성하라

--- app-server/src/main/java/com/growth/task/pomodoro/dto/request/PomodoroAddRequest.java
+++ app-server/src/main/java/com/growth/task/pomodoro/dto/request/PomodoroAddRequest.java
@@ -1,2 +1,36 @@
-package com.growth.task.pomodoro.dto.request;public class PomodoroAddRequest {
+package com.growth.task.pomodoro.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.growth.task.pomodoro.domain.Pomodoros;
+import com.growth.task.todo.domain.Todos;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PomodoroAddRequest {
+
+    private int performCount;
+    @NotNull(message = "계획 횟수는 필수 입력 값입니다.")
+    private int planCount;
+
+
+    @Builder
+    public PomodoroAddRequest(int performCount, int planCount) {
+        this.performCount = performCount;
+        this.planCount = planCount;
+    }
+
+    public Pomodoros toEntity(Todos todos) {
+        return Pomodoros.builder()
+                .todo(todos)
+                .performCount(this.performCount)
+                .planCount(this.planCount)
+                .build();
+    }
 }
