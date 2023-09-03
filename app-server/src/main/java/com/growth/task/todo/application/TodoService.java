842cad47256dc9831a917e5662329ceb76a1d369
caoyu <choyumin01@gmail.com>
2023-09-03 17:46:02 +0900

    TodoService 클래스에 Todos 를 저장할 save 메서드를 작성하라

--- app-server/src/main/java/com/growth/task/todo/application/TodoService.java
+++ app-server/src/main/java/com/growth/task/todo/application/TodoService.java
@@ -1,2 +1,30 @@
-package com.growth.task.todo.application;public class TodoService {
+package com.growth.task.todo.application;
+
+import com.growth.task.task.domain.Tasks;
+import com.growth.task.task.repository.TasksRepository;
+import com.growth.task.todo.domain.Todos;
+import com.growth.task.todo.dto.request.TodoAddRequest;
+import com.growth.task.todo.exception.TaskNotFoundException;
+import com.growth.task.todo.repository.TodosRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+    private TodosRepository todosRepository;
+
+    private TasksRepository tasksRepository;
+
+    public TodoService(TodosRepository todosRepository, TasksRepository tasksRepository) {
+        this.todosRepository = todosRepository;
+        this.tasksRepository = tasksRepository;
+    }
+
+    public Todos save(TodoAddRequest todoAddRequest) {
+        Long taskId = todoAddRequest.getTaskId();
+        Tasks tasks = tasksRepository.findById(taskId)
+                .orElseThrow(() -> new TaskNotFoundException(taskId));
+
+        Todos todos = todoAddRequest.toEntity(tasks);
+        return todosRepository.save(todos);
+    }
 }
