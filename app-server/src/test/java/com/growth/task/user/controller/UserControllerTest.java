754de8822e64d1e624f41893893766fbac9303be
kyuwon lee <86951640+kyuwon53@users.noreply.github.com>
2023-09-05 19:19:44 +0900

    사용자는 이름과 비밀번호로 회원가입할 수 있다 (#24)
    * 사용자 회원가입 DTO를 생성하라
    
    * 이름이 이미 존재하는 경우 예외를 던지고 409를 응답하라
    
    * 이름으로 사용자가 존재하는지 조회하라
    
    * 이름과 비밀번호로 사용자를 회원가입 시켜라
    
    * report 경로를 수정하라
    
    * test 실패시 디버깅을 위한 report 설정을 제거하라
    
    * app-server로 이동후 그래들 권한을 부여하라
    
    * app-server로 이동후 그래들 테스트를 실행하라
    
    * 리턴 타입을 boolean으로 수정하라
    
    * 이미 존재하는 이름인지 확인하는 코드를 메소드로 분리하라
    
    * 테스트 설명을 Context에 맞게 수정하라
    
    ---------
    
    Co-authored-by: leekyuwon <kyuwon.lee@makinarocks.ai>

---
+++ app-server/src/test/java/com/growth/task/user/controller/UserControllerTest.java
@@ -0,0 +1,108 @@
+package com.growth.task.user.controller;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.growth.task.user.domain.Users;
+import com.growth.task.user.domain.UsersRepository;
+import com.growth.task.user.dto.UserSignUpRequest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+class UserControllerTest {
+    @Autowired
+    private UsersRepository usersRepository;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext wac;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build();
+        objectMapper.registerModule(new JavaTimeModule())
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    @AfterEach
+    void cleanUp() {
+        usersRepository.deleteAll();
+    }
+
+    @Nested
+    @DisplayName("POST /api/v1/users 요청은")
+    class Describe_POST {
+        private ResultActions subject(UserSignUpRequest request) throws Exception {
+            return mockMvc.perform(post("/api/v1/users")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(APPLICATION_JSON)
+                    .accept(APPLICATION_JSON)
+            );
+        }
+
+        @Nested
+        @DisplayName("name과 password가 주어지면")
+        class Context_with_name_and_password {
+            private final UserSignUpRequest request = UserSignUpRequest.builder()
+                    .name("grow")
+                    .password("password")
+                    .build();
+
+            @Test
+            @DisplayName("User를 생성하고, 201을 응답한다")
+            void it_response_201() throws Exception {
+                final ResultActions resultActions = subject(request);
+
+                resultActions.andExpect(status().isCreated())
+                        .andExpect(jsonPath("name").value("grow"))
+                ;
+            }
+        }
+
+        @Nested
+        @DisplayName("이미 존재하는 name과 password가 주어지면")
+        class Context_with_exist_name_and_password {
+            @BeforeEach
+            void setUp() {
+                usersRepository.save(Users.builder().name("grow").password("password").build());
+            }
+
+            private final UserSignUpRequest request = UserSignUpRequest.builder()
+                    .name("grow")
+                    .password("password")
+                    .build();
+
+            @Test
+            @DisplayName("Conflict 409을 응답한다")
+            void it_response_409() throws Exception {
+                final ResultActions resultActions = subject(request);
+
+                resultActions.andExpect(status().isConflict())
+                ;
+            }
+        }
+    }
+}
