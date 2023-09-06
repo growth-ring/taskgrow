613c235407e0252acc05935b2d78b65a750453b0
JiHo Kim <kr36510@naver.com>
2023-09-06 23:59:42 +0900

    캘린더 수정

--- app-client/src/components/Calendar/Calendar.tsx
+++ app-client/src/components/Calendar/Calendar.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import moment from 'moment';
-import styled from 'styled-components';
-
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './Calendar.css';
-
-import { useTodosStore } from '../../store/todos';
+import styled from 'styled-components';
 
 import good from '../../assets/good.png';
+import { useTodosStore } from '../../store/todos';
 
 const Todo = styled.div`
   width: 50px;
@@ -27,37 +24,46 @@ const Todo = styled.div`
 
 const TaskCalendar = () => {
   const navigate = useNavigate();
-
   const { setToday } = useTodosStore();
-  const [value, onChange] = useState<Date>(new Date());
-  const day = moment(value).format('YYYY-MM-DD');
-  const currDate = new Date();
+  
+  //TODO: task요청시 현재 날짜의 +7 , -7 task 요청
+// const today = new Date();
+// const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+
+// const sevenDaysAgo = new Date(firstDayOfMonth);
+// sevenDaysAgo.setDate(firstDayOfMonth.getDate() - 7);
+
+// const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+
+// const sevenDaysLater = new Date(lastDayOfMonth);
+// sevenDaysLater.setDate(lastDayOfMonth.getDate() + 7);
+
+// console.log('7일 이전 날짜:', sevenDaysAgo.toLocaleDateString());
+// console.log('7일 이후 날짜:', sevenDaysLater.toLocaleDateString());
 
   const mark = ['2023-08-25', '2023-08-31', '2022-11-22', '2022-11-10'];
 
-  const handleTodayClick = (day) => {
+  const handleTodayClick = (day: Date) => {
     setToday(moment(day).format('YYYY-MM-DD'));
     navigate('/todos');
-  }
+  };
 
   return (
     <Calendar
       onClickDay={handleTodayClick}
-      onChange={onChange}
-      value={value}
       locale="ko-KO"
       next2Label={null}
       prev2Label={null}
       formatDay={(locale, date) => moment(date).format('D')}
       tileContent={({ date }) => {
-        const html = [];
+        const html: JSX.Element[] = [];
 
         mark.forEach((day, i) => {
           if (day === moment(date).format('YYYY-MM-DD')) {
-            if(day === '2023-08-25') {
-              html.push(<img src={good} />);  
+            if (day === '2023-08-25') {
+              html.push(<img src={good} key={i} />);
             } else {
-              html.push(<Todo>12</Todo>);
+              html.push(<Todo key={i}>12</Todo>);
             }
           }
         });
