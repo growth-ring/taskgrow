7796de607d5b8e6e022cb878faafc13975535b1d
caoyu <choyumin01@gmail.com>
2023-09-06 17:34:31 +0900

    TodoService 내부의 TodoList 클래스 분리로 인한 TaskDetailService 에서 해당 메서드 사용 부분을 수정하라

--- app-server/src/main/java/com/growth/task/task/service/TaskDetailService.java
+++ app-server/src/main/java/com/growth/task/task/service/TaskDetailService.java
@@ -3,7 +3,7 @@ package com.growth.task.task.service;
 import com.growth.task.task.domain.Tasks;
 import com.growth.task.task.dto.TaskDetailResponse;
 import com.growth.task.task.repository.TasksRepository;
-import com.growth.task.todo.application.TodosService;
+import com.growth.task.todo.application.TodoListService;
 import com.growth.task.todo.exception.TaskNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -12,18 +12,18 @@ import java.util.List;
 @Service
 public class TaskDetailService {
     private final TasksRepository tasksRepository;
-    private final TodosService todosService;
+    private final TodoListService todoListService;
 
-    public TaskDetailService(TasksRepository tasksRepository, TodosService todosService) {
+    public TaskDetailService(TasksRepository tasksRepository, TodoListService todoListService) {
         this.tasksRepository = tasksRepository;
-        this.todosService = todosService;
+        this.todoListService = todoListService;
     }
 
     public TaskDetailResponse getTask(Long taskId) {
         Tasks task = tasksRepository.findById(taskId)
                 .orElseThrow(() -> new TaskNotFoundException(taskId));
 
-        List<String> todos = todosService.getTodosTop3ByTaskId(task.getTaskId());
+        List<String> todos = todoListService.getTodosTop3ByTaskId(task.getTaskId());
 
         return new TaskDetailResponse(task.getTaskId(), todos);
     }
