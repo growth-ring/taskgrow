d45933498805da40ad4f8fdf2c1fe554a9099ef2
caoyu <choyumin01@gmail.com>
2023-09-07 16:49:16 +0900

    TodoNotFoundException 에서 todoId 를 포함한 예외 메세지를 구현하라

--- app-server/src/main/java/com/growth/task/todo/exception/TodoNotFoundException.java
+++ app-server/src/main/java/com/growth/task/todo/exception/TodoNotFoundException.java
@@ -2,6 +2,10 @@ package com.growth.task.todo.exception;
 
 public class TodoNotFoundException extends RuntimeException {
     public TodoNotFoundException() {
-        super("This Todo is Not Found");
+        super("This Todo is Not Found.");
+    }
+
+    public TodoNotFoundException(Long todoId) {
+        super(String.format("This Todo is Not Found. %s", todoId));
     }
 }
