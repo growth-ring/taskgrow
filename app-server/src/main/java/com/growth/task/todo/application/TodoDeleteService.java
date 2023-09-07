cf32ea678a71d224f9062cdfc7dc768024d67012
caoyu <choyumin01@gmail.com>
2023-09-07 16:45:52 +0900

    todoId 를 이용한 삭제 기능 서비스 로직을 구현하라

--- app-server/src/main/java/com/growth/task/todo/application/TodoDeleteService.java
+++ app-server/src/main/java/com/growth/task/todo/application/TodoDeleteService.java
@@ -1,2 +1,22 @@
-package com.growth.task.todo.application;public class TodoDeleteService {
+package com.growth.task.todo.application;
+
+import com.growth.task.todo.domain.Todos;
+import com.growth.task.todo.exception.TodoNotFoundException;
+import com.growth.task.todo.repository.TodosRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoDeleteService {
+    private final TodosRepository todosRepository;
+
+    public TodoDeleteService(TodosRepository todosRepository) {
+        this.todosRepository = todosRepository;
+    }
+
+    public void deleteByTodoId(Long todoId) {
+        Todos todos = todosRepository.findById(todoId)
+                .orElseThrow(() -> new TodoNotFoundException(todoId));
+
+        todosRepository.delete(todos);
+    }
 }
