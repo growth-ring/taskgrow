b6ef51d842a24226c3d5e85490a628ce5a6cfdaa
caoyu <choyumin01@gmail.com>
2023-09-09 00:31:35 +0900

    Pomodoro 의 업데이트 요청을 위한 DTO 를 추가하라

--- app-server/src/main/java/com/growth/task/pomodoro/dto/request/PomodoroUpdateRequest.java
+++ app-server/src/main/java/com/growth/task/pomodoro/dto/request/PomodoroUpdateRequest.java
@@ -1,2 +1,32 @@
-package com.growth.task.pomodoro.dto.request;public class PomodoroUpdateRequest {
+package com.growth.task.pomodoro.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.growth.task.pomodoro.domain.Pomodoros;
+import com.growth.task.todo.domain.Todos;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PomodoroUpdateRequest {
+    private int performCount = 0;
+    private int planCount;
+
+    @Builder
+    public PomodoroUpdateRequest(int performCount, int planCount) {
+        this.performCount = performCount;
+        this.planCount = planCount;
+    }
+
+    public Pomodoros toEntity(Todos todos) {
+        return Pomodoros.builder()
+                .todo(todos)
+                .performCount(this.performCount)
+                .planCount(this.planCount)
+                .build();
+    }
 }
