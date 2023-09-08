53cfc11850a6b50bd03fa127e3dd3ad07067000f
caoyu <choyumin01@gmail.com>
2023-09-09 00:34:54 +0900

    TodoService 에 Todo 업데이트 메서드를 추가하라

--- app-server/src/main/java/com/growth/task/todo/application/TodoService.java
+++ app-server/src/main/java/com/growth/task/todo/application/TodoService.java
@@ -3,8 +3,11 @@ package com.growth.task.todo.application;
 import com.growth.task.task.domain.Tasks;
 import com.growth.task.task.repository.TasksRepository;
 import com.growth.task.todo.domain.Todos;
+import com.growth.task.todo.dto.composite.TodoAndPomodoroUpdateRequest;
 import com.growth.task.todo.dto.request.TodoAddRequest;
+import com.growth.task.todo.dto.request.TodoUpdateRequest;
 import com.growth.task.todo.exception.TaskNotFoundException;
+import com.growth.task.todo.exception.TodoNotFoundException;
 import com.growth.task.todo.repository.TodosRepository;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +29,22 @@ public class TodoService {
 
         Todos todos = todoAddRequest.toEntity(tasks);
         return todosRepository.save(todos);
+    }
+
+    public Todos update(Long todoId, TodoAndPomodoroUpdateRequest todoAndPomodoroUpdateRequest) {
+        Todos todos = todosRepository.findById(todoId)
+                .orElseThrow(() -> new TodoNotFoundException(todoId));
+
+        TodoUpdateRequest todoUpdateReq = todoAndPomodoroUpdateRequest.getTodoUpdateRequest();
+        if(todoUpdateReq.getTodo() != null) {
+            todos.updateTodo(todoUpdateReq.getTodo());
+        }
+        if(todoUpdateReq.getTaskId() != null) {
+            Tasks task = tasksRepository.findById(todoUpdateReq.getTaskId())
+                    .orElseThrow(() -> new TaskNotFoundException(todoUpdateReq.getTaskId()));
+            todos.updateTask(task);
+        }
+        todosRepository.save(todos);
+        return todos;
     }
 }
