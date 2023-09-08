9238a2fe562a6f1fe1f654487416c691c6fa4ca3
caoyu <choyumin01@gmail.com>
2023-09-09 00:40:04 +0900

    Pomodoro 업데이트에 대한 응답 DTO 를 구현하라

--- app-server/src/main/java/com/growth/task/pomodoro/dto/response/PomodoroUpdateResponse.java
+++ app-server/src/main/java/com/growth/task/pomodoro/dto/response/PomodoroUpdateResponse.java
@@ -1,2 +1,25 @@
-package com.growth.task.pomodoro.dto.response;public class PomodoroUpdateResponse {
+package com.growth.task.pomodoro.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.growth.task.pomodoro.domain.Pomodoros;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PomodoroUpdateResponse {
+    private int performCount;
+    private int planCount;
+
+    @Builder
+    public PomodoroUpdateResponse(int performCount, int planCount) {
+        this.performCount = performCount;
+        this.planCount = planCount;
+    }
+
+    public PomodoroUpdateResponse(Pomodoros pomodoros) {
+        this.performCount = pomodoros.getPerformCount();
+        this.planCount = pomodoros.getPlanCount();
+    }
 }
