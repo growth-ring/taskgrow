73f17eec7028a11dd23ab699d2130deee2a83e65
caoyu <choyumin01@gmail.com>
2023-09-09 00:30:50 +0900

    Todo 의 업데이트 요청을 위한 DTO 를 추가하라

--- app-server/src/main/java/com/growth/task/todo/dto/request/TodoUpdateRequest.java
+++ app-server/src/main/java/com/growth/task/todo/dto/request/TodoUpdateRequest.java
@@ -1,2 +1,33 @@
-package com.growth.task.todo.dto.request;public class TodoUpdateRequest {
+package com.growth.task.todo.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.growth.task.task.domain.Tasks;
+import com.growth.task.todo.domain.Todos;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Todo 업데이트 요청
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class TodoUpdateRequest {
+    private Long taskId;
+    private String todo;
+
+    @Builder
+    public TodoUpdateRequest(Long taskId, String todo) {
+        this.taskId = taskId;
+        this.todo = todo;
+    }
+
+    public Todos toEntity(Tasks tasks) {
+        return Todos.builder()
+                .task(tasks)
+                .build();
+    }
 }
