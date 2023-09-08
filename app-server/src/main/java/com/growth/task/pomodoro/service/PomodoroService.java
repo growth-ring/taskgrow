952c08fbff450810571273bf99be8d619e73175f
caoyu <choyumin01@gmail.com>
2023-09-09 00:35:41 +0900

    PomodoroService 에 Pomodoro 업데이트 메서드를 추가하라

--- app-server/src/main/java/com/growth/task/pomodoro/service/PomodoroService.java
+++ app-server/src/main/java/com/growth/task/pomodoro/service/PomodoroService.java
@@ -1,9 +1,11 @@
 package com.growth.task.pomodoro.service;
 
 import com.growth.task.pomodoro.domain.Pomodoros;
-import com.growth.task.pomodoro.domain.PomodorosRepository;
+import com.growth.task.pomodoro.repository.PomodorosRepository;
 import com.growth.task.pomodoro.dto.request.PomodoroAddRequest;
+import com.growth.task.pomodoro.dto.request.PomodoroUpdateRequest;
 import com.growth.task.todo.domain.Todos;
+import com.growth.task.todo.dto.composite.TodoAndPomodoroUpdateRequest;
 import com.growth.task.todo.exception.TodoNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +23,16 @@ public class PomodoroService {
         }
         Pomodoros pomodoros = pomodoroAddRequest.toEntity(todos);
         return pomodorosRepository.save(pomodoros);
+    }
+
+    public Pomodoros update(Long todoId, TodoAndPomodoroUpdateRequest todoAndPomodoroUpdateRequest) {
+        Pomodoros pomodoros = pomodorosRepository.findByTodo_TodoId(todoId);
+        PomodoroUpdateRequest pomodoroUpdateReq = todoAndPomodoroUpdateRequest.getPomodoroUpdateRequest();
+        if(pomodoroUpdateReq != null) {
+            pomodoros.updatePerformCount(pomodoroUpdateReq.getPerformCount());
+            pomodoros.updatePlanCount(pomodoroUpdateReq.getPlanCount());
+        }
+        pomodorosRepository.save(pomodoros);
+        return pomodoros;
     }
 }
