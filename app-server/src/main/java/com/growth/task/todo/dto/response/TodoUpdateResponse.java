8227ab981973391fd990b3f4bb1c4a5228e1564a
caoyu <choyumin01@gmail.com>
2023-09-09 00:39:17 +0900

    Todo 업데이트에 대한 응답 DTO 를 구현하라

--- app-server/src/main/java/com/growth/task/todo/dto/response/TodoUpdateResponse.java
+++ app-server/src/main/java/com/growth/task/todo/dto/response/TodoUpdateResponse.java
@@ -1,2 +1,29 @@
-package com.growth.task.todo.dto.response;public class TodoUpdateResponse {
+package com.growth.task.todo.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.growth.task.todo.domain.Todos;
+import com.growth.task.todo.enums.Status;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Todo 업데이튼 응답
+ */
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class TodoUpdateResponse {
+    private String todo;
+    private Status status;
+
+    @Builder
+    public TodoUpdateResponse(String todo, Status status) {
+        this.todo = todo;
+        this.status = status;
+    }
+
+    public TodoUpdateResponse(Todos todos) {
+        this.todo = todos.getTodo();
+        this.status = todos.updateStatus();
+    }
 }
