2270d316160156b673959ddbb211dd2763d68e36
JiHo Kim <kr36510@naver.com>
2023-09-13 00:27:37 +0900

    task 리스트 조회

--- app-client/src/components/Todos/HeaderDate.tsx
+++ app-client/src/components/Todos/HeaderDate.tsx
@@ -1,32 +1,49 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import moment from 'moment';
-import { useTodosStore } from '../../store/todos';
-import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
+import { useParams } from 'react-router-dom';
+import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
 
 const Arrow = styled.button`
-    margin: 0 100px;
+  margin: 0 100px;
 `;
 
 const Today = styled.div`
-    width: 180px;
-    text-align: center;
+  width: 180px;
+  text-align: center;
 `;
 
 const HeaderDate = () => {
-  const { today, setToday } = useTodosStore();
-  const [years, month, day] = today.split("-");
+  const { date } = useParams();
+  const navigate = useNavigate();
 
-  const handleShowDate = (date:string) => {
-    const todayDate = new Date(today);
-    const currentDate = date === 'previous' ? todayDate.getDate() - 1 : todayDate.getDate() + 1;
-    setToday(moment(todayDate.setDate(currentDate)).format('YYYY-MM-DD'));
+  if (!date) {
+    return null;
   }
+
+  const [years, month, day] = date.split('-');
+
+  const handleShowDate = (clickDate: string) => {
+    const todayDate = new Date(date);
+    const currentDate =
+      clickDate === 'previous'
+        ? todayDate.getDate() - 1
+        : todayDate.getDate() + 1;
+    const clickTodayDate = moment(todayDate.setDate(currentDate)).format(
+      'YYYY-MM-DD',
+    );
+    navigate(`/todos/${clickTodayDate}`);
+  };
 
   return (
     <>
-      <Arrow onClick={() => handleShowDate('previous')}><SlArrowLeft /></Arrow>
+      <Arrow onClick={() => handleShowDate('previous')}>
+        <SlArrowLeft />
+      </Arrow>
       <Today>{`${years}년 ${month}월 ${day}일`}</Today>
-      <Arrow onClick={() => handleShowDate('next')}><SlArrowRight /></Arrow>
+      <Arrow onClick={() => handleShowDate('next')}>
+        <SlArrowRight />
+      </Arrow>
     </>
   );
 };
