c5855229b4d03e21bd6d7aadd1acb357165c933e
caoyu <choyumin01@gmail.com>
2023-09-13 00:34:59 +0900

    PomodoroService 에 대한 update 테스트를 작성하라

--- app-server/src/test/java/com/growth/task/pomodoro/service/PomodoroServiceTest.java
+++ app-server/src/test/java/com/growth/task/pomodoro/service/PomodoroServiceTest.java
@@ -1,6 +1,7 @@
 package com.growth.task.pomodoro.service;
 
 import com.growth.task.pomodoro.domain.Pomodoros;
+import com.growth.task.pomodoro.dto.request.PomodoroUpdateRequest;
 import com.growth.task.pomodoro.repository.PomodorosRepository;
 import com.growth.task.pomodoro.dto.request.PomodoroAddRequest;
 import com.growth.task.todo.domain.Todos;
@@ -13,6 +14,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -78,6 +82,36 @@ class PomodoroServiceTest {
                 assertThrows(TodoNotFoundException.class, () -> {
                     pomodoroService.save(pomodoroAddRequest, todos);
                 });
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("PomodoroService 의 update 메서드는")
+    class Describe_update {
+
+        @Nested
+        @DisplayName("todoId 가 null 이 아닌 경우")
+        class Context_whenTodoIdExists {
+            private final PomodoroUpdateRequest pomodoroUpdateRequest = PomodoroUpdateRequest.builder()
+                    .planCount(PLANCOUNT1)
+                    .build();
+
+            private final Pomodoros pomodoros = Pomodoros.builder()
+                    .planCount(PLANCOUNT2)
+                    .build();
+
+            @BeforeEach
+            void setUp() {
+                given(pomodorosRepository.findByTodo_TodoId(TODO_ID1)).willReturn(pomodoros);
+            }
+
+            @Test
+            @DisplayName("pomodoroUpdateRequest 를 입력 받아 업데이트한다.")
+            void It_updateThePomodoro() {
+                Pomodoros response = pomodoroService.update(TODO_ID1, pomodoroUpdateRequest);
+
+                assertThat(response.getPlanCount()).isEqualTo(PLANCOUNT1);
             }
         }
     }
