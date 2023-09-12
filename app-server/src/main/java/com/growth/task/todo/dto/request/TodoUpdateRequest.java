67f1559d9038694d42d1e0421db270dfa53fdda7
caoyu <choyumin01@gmail.com>
2023-09-12 23:20:41 +0900

    TodoUpdateRequest 의 taskId 필드에 필수 입력 값 검증을 위한  어노테이션을 추가하라

--- app-server/src/main/java/com/growth/task/todo/dto/request/TodoUpdateRequest.java
+++ app-server/src/main/java/com/growth/task/todo/dto/request/TodoUpdateRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.growth.task.task.domain.Tasks;
 import com.growth.task.todo.domain.Todos;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TodoUpdateRequest {
+    @NotNull(message = "task 아이디는 필수 입력 값입니다.")
     private Long taskId;
     private String todo;
 
