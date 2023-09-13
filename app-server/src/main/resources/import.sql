0639686e27bf37ae6c18fbbcadc2f4cb9be1ed83
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-09-13 09:19:39 +0900

    초기 데이터 생성 sql문을 bulk insert가 아닌 단일 insert로 수정하라

--- app-server/src/main/resources/import.sql
+++ app-server/src/main/resources/import.sql
@@ -1,11 +1,9 @@
 -- user data 생성
-INSERT INTO users (name, password, created_at, updated_at)
-    VALUES
-     ('kyuwon','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-     ('yumin','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-     ('jiho','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-     ('user1','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-     ('user2','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-     ('user3','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-     ('user4','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-     ('user5','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO users (name, password, created_at, updated_at) VALUES ('kyuwon','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO users (name, password, created_at, updated_at) VALUES ('yumin','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO users (name, password, created_at, updated_at) VALUES ('jiho','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO users (name, password, created_at, updated_at) VALUES ('user1','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO users (name, password, created_at, updated_at) VALUES ('user2','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO users (name, password, created_at, updated_at) VALUES ('user3','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO users (name, password, created_at, updated_at) VALUES ('user4','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO users (name, password, created_at, updated_at) VALUES ('user5','1234',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
