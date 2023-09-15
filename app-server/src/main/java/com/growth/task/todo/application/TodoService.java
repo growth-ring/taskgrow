779d8fb17cb614608611c8e0bdcc6d73f9e56979
caoyu <choyumin01@gmail.com>
2023-09-15 18:17:43 +0900

    TodoService 의 update 메서드에 status 수정 로직을 작성하라

--- app-server/src/main/java/com/growth/task/todo/application/TodoService.java
+++ app-server/src/main/java/com/growth/task/todo/application/TodoService.java
@@ -34,14 +34,12 @@ public class TodoService {
         Todos todos = todosRepository.findById(todoId)
                 .orElseThrow(() -> new TodoNotFoundException(todoId));
 
-        if(todoUpdateRequest.getTodo() != null) {
+        if (todoUpdateRequest.getTodo() != null) {
             todos.updateTodo(todoUpdateRequest.getTodo());
         }
-
-        Tasks task = tasksRepository.findById(todoUpdateRequest.getTaskId())
-                .orElseThrow(() -> new TaskNotFoundException(todoUpdateRequest.getTaskId()));
-        todos.updateTask(task);
-
+        if (todoUpdateRequest.getStatus() != null) {
+            todos.updateStatus(todoUpdateRequest.getStatus());
+        }
         todosRepository.save(todos);
 
         return todos;
