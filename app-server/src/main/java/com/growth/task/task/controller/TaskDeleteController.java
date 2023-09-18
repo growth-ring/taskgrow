c072d215ad5e880b3656466aec6c36b107c35d9b
kyuwon lee <86951640+kyuwon53@users.noreply.github.com>
2023-09-18 15:12:05 +0900

    Swagger를 설정하라 (#49)
    * swagger를 설정하라
    
    * 스웨거에서 같은 도메인끼리 그룹화하라
    
    ---------
    
    Co-authored-by: leekyuwon <kyuwon.lee@makinarocks.ai>

--- app-server/src/main/java/com/growth/task/task/controller/TaskDeleteController.java
+++ app-server/src/main/java/com/growth/task/task/controller/TaskDeleteController.java
@@ -1,6 +1,8 @@
 package com.growth.task.task.controller;
 
 import com.growth.task.task.service.TaskDeleteService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.NO_CONTENT;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/tasks")
+@Tag(name = "Task", description = "Task API Document")
 public class TaskDeleteController {
     private final TaskDeleteService taskDeleteService;
 
--- app-server/src/main/java/com/growth/task/task/controller/TaskListController.java
+++ app-server/src/main/java/com/growth/task/task/controller/TaskListController.java
@@ -3,6 +3,7 @@ package com.growth.task.task.controller;
 import com.growth.task.task.dto.TaskListRequest;
 import com.growth.task.task.dto.TaskListResponse;
 import com.growth.task.task.service.TaskListService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import static org.springframework.http.HttpStatus.OK;
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1/tasks")
+@Tag(name = "Task", description = "Task API Document")
 public class TaskListController {
     private final TaskListService taskListService;
 
