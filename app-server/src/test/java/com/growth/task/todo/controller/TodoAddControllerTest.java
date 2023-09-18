172d2673d884eb2da3f0b781888bd98bc11c21aa
caoyu <choyumin01@gmail.com>
2023-09-18 22:35:54 +0900

    TodoAddControllerTest 를 작성하라

--- app-server/src/test/java/com/growth/task/todo/controller/TodoAddControllerTest.java
+++ app-server/src/test/java/com/growth/task/todo/controller/TodoAddControllerTest.java
@@ -1,53 +1,76 @@
 package com.growth.task.todo.controller;
 
-import com.growth.task.todo.application.TodoListService;
-import com.growth.task.todo.dto.response.TodoListResponse;
-import com.growth.task.todo.enums.Status;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.growth.task.pomodoro.dto.request.PomodoroAddRequest;
+import com.growth.task.todo.application.TodoAddService;
+import com.growth.task.todo.dto.composite.TodoAndPomodoroAddRequest;
+import com.growth.task.todo.dto.composite.TodoAndPomodoroAddResponse;
+import com.growth.task.todo.dto.request.TodoAddRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(TodoAddController.class)
 class TodoAddControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
-    private TodoListService todoListService;
+    private TodoAddService todoAddService;
+
+    @BeforeEach
+    public void setUp() {
+        TodoAndPomodoroAddResponse response = TodoAndPomodoroAddResponse.builder()
+                .todoId(1L)
+                .taskId(0L)
+                .todo("스터디 하기")
+                .status("대기중")
+                .performCount(2)
+                .planCount(5)
+                .build();
+
+        when(todoAddService.save(Mockito.any(TodoAndPomodoroAddRequest.class))).thenReturn(response);
+    }
 
     @Test
-    public void getTodosTest() throws Exception {
-        TodoListResponse todo1 = new TodoListResponse(1L, 1L, "디자인패턴의 아름다움 스터디", Status.READY, 1, 2);
-        TodoListResponse todo2 = new TodoListResponse(2L, 1L, "프로젝트 진행하기", Status.PROGRESS, 1, 2);
-        List<TodoListResponse> todoList = Arrays.asList(todo1, todo2);
-        when(todoListService.getTodosByTaskId(1L)).thenReturn(todoList);
+    public void testCreateTodoAndPomodoro() throws Exception {
+        // 요청 데이터 생성
+        TodoAddRequest todoAddRequest = TodoAddRequest.builder()
+                .taskId(0L)
+                .todo("디자인패턴의 아름다움 스터디")
+                .build();
+        PomodoroAddRequest pomodoroAddRequest = PomodoroAddRequest.builder()
+                .performCount(2)
+                .planCount(5)
+                .build();
+        TodoAndPomodoroAddRequest request = TodoAndPomodoroAddRequest.builder()
+                .todoAddRequest(todoAddRequest)
+                .pomodoroAddRequest(pomodoroAddRequest)
+                .build();
 
-        mockMvc.perform(get("/api/v1/todos").param("taskId", "1"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].todoId").value(1L))
-                .andExpect(jsonPath("$[0].taskId").value(1L))
-                .andExpect(jsonPath("$[0].todo").value("디자인패턴의 아름다움 스터디"))
-                .andExpect(jsonPath("$[0].status").value("READY"))
-                .andExpect(jsonPath("$[0].performCount").value(1))
-                .andExpect(jsonPath("$[0].planCount").value(2))
-                .andExpect(jsonPath("$[1].todoId").value(2L))
-                .andExpect(jsonPath("$[1].taskId").value(1L))
-                .andExpect(jsonPath("$[1].todo").value("프로젝트 진행하기"))
-                .andExpect(jsonPath("$[1].status").value("PROGRESS"))
-                .andExpect(jsonPath("$[1].performCount").value(1))
-                .andExpect(jsonPath("$[1].planCount").value(2));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/todos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.todo_id").value(1L))
+                .andExpect(jsonPath("$.task_id").value(0L))
+                .andExpect(jsonPath("$.todo").value("스터디 하기"))
+                .andExpect(jsonPath("$.status").value("대기중"))
+                .andExpect(jsonPath("$.perform_count").value(2))
+                .andExpect(jsonPath("$.plan_count").value(5));
     }
 }
