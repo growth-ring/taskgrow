f14bf65298ac0d793ca8c2ea67e8ce1e99fc1471
choyumin <choyumin01@gmail.com>
2023-09-18 18:25:04 +0900

    TodoAddControllerTest 를 작성하라

---
+++ app-server/src/test/java/com/growth/task/todo/controller/TodoAddControllerTest.java
@@ -0,0 +1,53 @@
+package com.growth.task.todo.controller;
+
+import com.growth.task.todo.application.TodoListService;
+import com.growth.task.todo.dto.response.TodoListResponse;
+import com.growth.task.todo.enums.Status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TodoAddControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TodoListService todoListService;
+
+    @Test
+    public void getTodosTest() throws Exception {
+        TodoListResponse todo1 = new TodoListResponse(1L, 1L, "디자인패턴의 아름다움 스터디", Status.READY, 1, 2);
+        TodoListResponse todo2 = new TodoListResponse(2L, 1L, "프로젝트 진행하기", Status.PROGRESS, 1, 2);
+        List<TodoListResponse> todoList = Arrays.asList(todo1, todo2);
+        when(todoListService.getTodosByTaskId(1L)).thenReturn(todoList);
+
+        mockMvc.perform(get("/api/v1/todos").param("taskId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].todoId").value(1L))
+                .andExpect(jsonPath("$[0].taskId").value(1L))
+                .andExpect(jsonPath("$[0].todo").value("디자인패턴의 아름다움 스터디"))
+                .andExpect(jsonPath("$[0].status").value("READY"))
+                .andExpect(jsonPath("$[0].performCount").value(1))
+                .andExpect(jsonPath("$[0].planCount").value(2))
+                .andExpect(jsonPath("$[1].todoId").value(2L))
+                .andExpect(jsonPath("$[1].taskId").value(1L))
+                .andExpect(jsonPath("$[1].todo").value("프로젝트 진행하기"))
+                .andExpect(jsonPath("$[1].status").value("PROGRESS"))
+                .andExpect(jsonPath("$[1].performCount").value(1))
+                .andExpect(jsonPath("$[1].planCount").value(2));
+    }
+}
