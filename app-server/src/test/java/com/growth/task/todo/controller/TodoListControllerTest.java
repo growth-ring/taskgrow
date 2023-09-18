2809cdcfae9dfad614899ab764490985957b1cba
choyumin <choyumin01@gmail.com>
2023-09-18 18:31:08 +0900

    TodoListControlTodoListControllerIntegrationTest 에서 TodoListControllerTest 로 테스트 클래스명을 변경하라

--- app-server/src/test/java/com/growth/task/todo/controller/TodoListControllerTest.java
+++ app-server/src/test/java/com/growth/task/todo/controller/TodoListControllerTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class TodoListControllerIntegrationTest {
+class TodoListControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
