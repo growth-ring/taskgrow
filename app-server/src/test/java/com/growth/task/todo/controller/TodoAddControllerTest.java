5e7543d7797df8f38a28ac994aa5664210e5ad9f
조씨 <73353809+caoyu-dev@users.noreply.github.com>
2023-09-19 22:36:03 +0900

    TodoAdd API 를 수정하라 (#54)
    * TodoAndPomodoroAddRequest 의 @JsonCreator 생성자를 추가하라
    
    - @JsonProperty 를 이용해 JSON 매핑과 동시에 객체 초기화
    - TodoAddRequest 와 PomodoroAddRequest 객체를 생성자에서 직접 초기화
    
    * TodoAddPomodoroAddResponse 구조를 변경하라
    
    - 필드로 TodoAddResponse 와 PomodoroAddResponse 객체 대신 관련 속성들을 직접 선언
    - TodoAddResponse 와 PomodoroAddResponse 를 매개변수로 받는 생성자 추가
    - TodoAddResponse와 PomodoroAddResponse 객체를 이용하여 TodoAndPomodoroAddResponse 객체를 생성하는 정적 메서드 추가
    - Enum 의 status 필드 문자열 변환 로직 추가
    
    * TodoAddServiceTest 를 수정하라
    
    * TodoAddControllerTest 를 작성하라
    
    * EOF 추가하라
    
    * TodoListControlTodoListControllerIntegrationTest 에서 TodoListControllerTest 로 테스트 클래스명을 변경하라
    
    * TodoAddRequest 의 toEntity 메서드에 todo 필드를 추가하라
    
    * TodoAddControllerTest 를 작성하라
    
    * TodoAndPomodoroAddRequest 에서 JSON 직렬화 무시용 @JsonIgnore 를 추가하라
    
    * 를 유지하면서, 직렬화될 추가적인 정보를 제공하기 위해 직렬화 전용 getter 를 추가하라
    
    * TodoAddRequest 에 status 초기화 로직을 추가하라
    
    - Todo 생성 요청 DTO 인 TodoAddRequest 에 할 일의 상태를 초기화하는 로직 추가
    - 기본 상태는 'READY'로 설정
    
    * TodoAddControllerTest 의 상태 값  을  로 수정하라
    
    * 구체적인 import 로 변경하라
    
    * TodoAndPomodoroAddRequest JSON 응답에서 사용할 프로퍼티명을 명시적으로 추가하라

---
+++ app-server/src/test/java/com/growth/task/todo/controller/TodoAddControllerTest.java
@@ -0,0 +1,76 @@
+package com.growth.task.todo.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.growth.task.pomodoro.dto.request.PomodoroAddRequest;
+import com.growth.task.todo.application.TodoAddService;
+import com.growth.task.todo.dto.composite.TodoAndPomodoroAddRequest;
+import com.growth.task.todo.dto.composite.TodoAndPomodoroAddResponse;
+import com.growth.task.todo.dto.request.TodoAddRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TodoAddController.class)
+class TodoAddControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private TodoAddService todoAddService;
+
+    @BeforeEach
+    public void setUp() {
+        TodoAndPomodoroAddResponse response = TodoAndPomodoroAddResponse.builder()
+                .todoId(1L)
+                .taskId(0L)
+                .todo("스터디 하기")
+                .status("ready")
+                .performCount(2)
+                .planCount(5)
+                .build();
+
+        when(todoAddService.save(Mockito.any(TodoAndPomodoroAddRequest.class))).thenReturn(response);
+    }
+
+    @Test
+    public void testCreateTodoAndPomodoro() throws Exception {
+        // 요청 데이터 생성
+        TodoAddRequest todoAddRequest = TodoAddRequest.builder()
+                .taskId(0L)
+                .todo("디자인패턴의 아름다움 스터디")
+                .build();
+        PomodoroAddRequest pomodoroAddRequest = PomodoroAddRequest.builder()
+                .performCount(2)
+                .planCount(5)
+                .build();
+        TodoAndPomodoroAddRequest request = TodoAndPomodoroAddRequest.builder()
+                .todoAddRequest(todoAddRequest)
+                .pomodoroAddRequest(pomodoroAddRequest)
+                .build();
+
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/todos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.todo_id").value(1L))
+                .andExpect(jsonPath("$.task_id").value(0L))
+                .andExpect(jsonPath("$.todo").value("스터디 하기"))
+                .andExpect(jsonPath("$.status").value("ready"))
+                .andExpect(jsonPath("$.perform_count").value(2))
+                .andExpect(jsonPath("$.plan_count").value(5));
+    }
+}
