c00f1e1072f287cd773884d2e7884786371dd614
caoyu <choyumin01@gmail.com>
2023-09-26 23:45:42 +0900

    Pomodoro 예외 핸들러를 삭제하라

--- app-server/src/main/java/com/growth/task/pomodoro/exception/PomodoroExceedPlanCountException.java
+++
@@ -1,7 +0,0 @@
-package com.growth.task.pomodoro.exception;
-
-public class PomodoroExceedPlanCountException extends RuntimeException {
-    public PomodoroExceedPlanCountException() {
-        super("Pomodoro perform count cannot be greater than the plan count.");
-    }
-}
