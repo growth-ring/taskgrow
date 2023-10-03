1b1f988ff53ead769200deaa8bb5c40d3f16aa17
leekyuwon <kyuwoon369@gmail.com>
2023-10-03 15:37:36 +0900

    Swagger Api를 카테코리별로 분류하라

--- app-server/src/main/java/com/growth/task/todo/controller/TodoDeleteController.java
+++ app-server/src/main/java/com/growth/task/todo/controller/TodoDeleteController.java
@@ -1,12 +1,14 @@
 package com.growth.task.todo.controller;
 
 import com.growth.task.todo.application.TodoDeleteService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
 @RequestMapping("/api/v1/todos")
+@Tag(name = "Todo", description = "Todo API Document")
 public class TodoDeleteController {
 
     private final TodoDeleteService todoDeleteService;
--- app-server/src/main/java/com/growth/task/todo/controller/TodoUpdateController.java
+++ app-server/src/main/java/com/growth/task/todo/controller/TodoUpdateController.java
@@ -4,6 +4,7 @@ import com.growth.task.todo.application.TodoService;
 import com.growth.task.todo.domain.Todos;
 import com.growth.task.todo.dto.request.TodoUpdateRequest;
 import com.growth.task.todo.dto.response.TodoUpdateResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,7 @@ import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/v1/todos")
+@Tag(name = "Todo", description = "Todo API Document")
 public class TodoUpdateController {
 
     private final TodoService todoService;
