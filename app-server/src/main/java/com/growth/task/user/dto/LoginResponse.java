d66a56a823aeaf0c9e764f662faf9b4cf657f778
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-10-25 08:00:58 +0900

    로그인 응답에 id를 추가하라

--- app-server/src/main/java/com/growth/task/user/dto/LoginResponse.java
+++ app-server/src/main/java/com/growth/task/user/dto/LoginResponse.java
@@ -1,14 +1,22 @@
 package com.growth.task.user.dto;
 
+import com.growth.task.user.domain.Users;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
 public class LoginResponse {
+    private Long id;
     private String name;
 
-    public LoginResponse(String name) {
+    public LoginResponse(Long id, String name) {
+        this.id = id;
         this.name = name;
+    }
+
+    public LoginResponse(Users users) {
+        this.id = users.getUserId();
+        this.name = users.getName();
     }
 }
