818f71110c356ec31bb4c3191a60539bac37d448
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-10-27 14:14:11 +0900

    테스트 이름을 수정하라

--- app-server/src/test/java/com/growth/task/review/controller/ReviewDetailControllerTest.java
+++ app-server/src/test/java/com/growth/task/review/controller/ReviewDetailControllerTest.java
@@ -30,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@DisplayName("ReviewAddController")
+@DisplayName("ReviewDetailController")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 class ReviewDetailControllerTest {
