a6eb8247dca63d725dfba2653e6738b6ccb4fd48
leekyuwon <kyuwoon369@gmail.com>
2023-11-01 08:19:37 +0900

    테스크 리스트 조회시 기분점수를 가져오고 회고가 없다면 -1을 응답하는지 테스트하라

--- app-server/src/test/java/com/growth/task/task/controller/TaskListControllerTest.java
+++ app-server/src/test/java/com/growth/task/task/controller/TaskListControllerTest.java
@@ -3,6 +3,8 @@ package com.growth.task.task.controller;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.growth.task.review.domain.Review;
+import com.growth.task.review.service.ReviewRepository;
 import com.growth.task.task.domain.Tasks;
 import com.growth.task.task.dto.TaskListRequest;
 import com.growth.task.task.repository.TasksRepository;
@@ -46,6 +48,8 @@ class TaskListControllerTest {
     private UsersRepository usersRepository;
     @Autowired
     private TodosRepository todosRepository;
+    @Autowired
+    private ReviewRepository reviewRepository;
     private final ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
     private MockMvc mockMvc;
@@ -90,8 +94,17 @@ class TaskListControllerTest {
         );
     }
 
+    private Review createReview(Tasks tasks, String contents, Integer feelingsScore) {
+        return reviewRepository.save(Review.builder()
+                .tasks(tasks)
+                .contents(contents)
+                .feelingsScore(feelingsScore)
+                .build());
+    }
+
     @AfterEach
     void cleanUp() {
+        reviewRepository.deleteAll();
         todosRepository.deleteAll();
         tasksRepository.deleteAll();
         usersRepository.deleteAll();
@@ -130,6 +143,8 @@ class TaskListControllerTest {
             getTodo(task2, "파이썬 코딩의 기술 읽기", Status.DONE);
             getTodo(task2, "투스쿱 장고 읽기", Status.DONE);
             getTodo(task3, "운동하기", Status.READY);
+
+            createReview(task1, "회고를 작성합니다.", 7);
         }
 
         @Nested
@@ -152,15 +167,19 @@ class TaskListControllerTest {
                         .andExpect(jsonPath("$[0].task_date").value(equalTo("2023-08-28")))
                         .andExpect(jsonPath("$[0].todos.remain").value(equalTo(0)))
                         .andExpect(jsonPath("$[0].todos.done").value(equalTo(0)))
+                        .andExpect(jsonPath("$[0].feelings_score").value(equalTo(-1)))
                         .andExpect(jsonPath("$[1].task_date").value(equalTo("2023-08-29")))
                         .andExpect(jsonPath("$[1].todos.remain").value(equalTo(3)))
                         .andExpect(jsonPath("$[1].todos.done").value(equalTo(2)))
+                        .andExpect(jsonPath("$[1].feelings_score").value(equalTo(7)))
                         .andExpect(jsonPath("$[2].task_date").value(equalTo("2023-08-30")))
                         .andExpect(jsonPath("$[2].todos.remain").value(equalTo(2)))
                         .andExpect(jsonPath("$[2].todos.done").value(equalTo(2)))
+                        .andExpect(jsonPath("$[2].feelings_score").value(equalTo(-1)))
                         .andExpect(jsonPath("$[3].task_date").value(equalTo("2023-08-31")))
                         .andExpect(jsonPath("$[3].todos.remain").value(equalTo(1)))
                         .andExpect(jsonPath("$[3].todos.done").value(equalTo(0)))
+                        .andExpect(jsonPath("$[3].feelings_score").value(equalTo(-1)))
                 ;
             }
         }
