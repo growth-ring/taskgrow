fff13f3022a554eff26e91bbeed0f1604c46714c
JiHo Kim <kr36510@naver.com>
2023-10-31 11:43:02 +0900

    회고 삭제

--- app-client/src/components/Review/UserReview.tsx
+++ app-client/src/components/Review/UserReview.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { BsTrash3, BsCheckCircleFill } from 'react-icons/bs';
 import { useTask } from '../../store/task';
 import { useReviewStore } from '../../store/review';
-import { addReview, getReview } from '../../services/review';
+import { addReview, getReview, deleteReview } from '../../services/review';
 
 const Button = styled.button`
   &:hover {
@@ -50,7 +50,8 @@ const ButtonBox = styled.div`
 
 const UserReview = () => {
   const { selectedTaskId } = useTask();
-  const { feelingsScore, setFeelingsScore, setReviewId } = useReviewStore();
+  const { feelingsScore, setFeelingsScore, setReviewId, reviewId } =
+    useReviewStore();
   const [inputValue, setInputValue] = useState<string>('');
 
   const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -65,7 +66,17 @@ const UserReview = () => {
     };
     const isAddReview = await addReview(reviewData);
     if (isAddReview) {
+      setReviewId(isAddReview.review_id);
       alert('회고 추가 되었습니다.');
+    }
+  };
+
+  const handleDeleteReview = async () => {
+    const isDelete = await deleteReview(reviewId);
+    if (isDelete) {
+      alert('회고 삭제 되었습니다.');
+      setReviewId(0);
+      setInputValue('');
     }
   };
 
@@ -86,7 +97,7 @@ const UserReview = () => {
     <>
       <Review value={inputValue} onChange={handleInputChange}></Review>
       <ButtonBox>
-        <Button style={{ margin: '0 1rem' }}>
+        <Button onClick={handleDeleteReview} style={{ margin: '0 1rem' }}>
           <BsTrash3 />
         </Button>
         <Button onClick={handleAddReview}>
