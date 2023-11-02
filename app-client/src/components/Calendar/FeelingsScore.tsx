c50556af693758307c82aabcaa9db1c664e0da1c
JiHo Kim <kr36510@naver.com>
2023-11-03 01:37:15 +0900

    모바일에서의 task에서의 회고 표시 바꿔라

--- app-client/src/components/Calendar/FeelingsScore.tsx
+++ app-client/src/components/Calendar/FeelingsScore.tsx
@@ -1,11 +1,51 @@
 import styled from 'styled-components';
 
+import cry from '../../assets/cry.png';
+import sad from '../../assets/sad.png';
+import soso from '../../assets/soso.png';
+import good from '../../assets/good.png';
+import nice from '../../assets/nice.png';
+import happy from '../../assets/happy.png';
+
 import pc_cry from '../../assets/pc_cry.png';
 import pc_sad from '../../assets/pc_sad.png';
 import pc_soso from '../../assets/pc_soso.png';
 import pc_good from '../../assets/pc_good.png';
 import pc_nice from '../../assets/pc_nice.png';
 import pc_happy from '../../assets/pc_happy.png';
+
+interface ImageProps {
+  $bg: string;
+}
+
+const Image = styled.img<ImageProps>`
+  background: ${(props) => props.$bg};
+
+  @media (max-width: 767px) {
+    content: url(${(props) =>
+      props.$bg === 'red'
+        ? cry
+        : props.$bg === 'orange'
+        ? sad
+        : props.$bg === '#fcee1e'
+        ? soso
+        : props.$bg === '#0ba322'
+        ? good
+        : props.$bg === '#338bff'
+        ? nice
+        : happy});
+  }
+
+  @media (min-width: 768px) and (max-width: 1023px) {
+    width: 150px;
+    height: 80px;
+  }
+
+  @media (min-width: 1024px) {
+    width: 160px;
+    height: 90px;
+  }
+`;
 
 const FeelingsScore = ({ score }: { score: number }) => {
   let feeling = '';
@@ -31,21 +71,7 @@ const FeelingsScore = ({ score }: { score: number }) => {
     bg = '#89d1fa';
   }
 
-  const Image = styled.img`
-    background: ${bg};
-
-    @media (min-width: 768px) and (max-width: 1023px) {
-      width: 150px;
-      height: 80px;
-    }
-
-    @media (min-width: 1024px) {
-      width: 160px;
-      height: 90px;
-    }
-  `;
-
-  return <Image src={feeling} />;
+  return <Image src={feeling} $bg={bg} />;
 };
 
 export default FeelingsScore;
