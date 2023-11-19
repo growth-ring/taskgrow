db5087386abd86107e5333500041187cdcb79fe4
leekyuwon <kyuwoon369@gmail.com>
2023-11-19 23:22:17 +0900

    스네이크 형식 파라미터를 받도록 수정하라

--- app-server/src/main/java/com/growth/task/todo/dto/TodoStatsRequest.java
+++ app-server/src/main/java/com/growth/task/todo/dto/TodoStatsRequest.java
@@ -17,8 +17,8 @@ public class TodoStatsRequest {
     private LocalDate endDate;
 
     @Builder
-    public TodoStatsRequest(LocalDate startDate, LocalDate endDate) {
-        this.startDate = startDate;
-        this.endDate = endDate;
+    public TodoStatsRequest(LocalDate start_date, LocalDate end_date) {
+        this.startDate = start_date;
+        this.endDate = end_date;
     }
 }
