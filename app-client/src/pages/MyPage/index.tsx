59b251d22b2a2b9709a05c192ccc3b37eefd8732
JiHo Kim <kr36510@naver.com>
2023-11-26 17:05:58 +0900

    통계 월단위 헤더

--- app-client/src/pages/MyPage/index.tsx
+++ app-client/src/pages/MyPage/index.tsx
@@ -1,8 +1,22 @@
 import styled from 'styled-components';
 import Header from '../../components/Menu/Header';
+import HeaderDate from '../../components/Stats/HeaderDate';
 
 const Line = styled.div`
   background-color: #f5f5f5;
+`;
+
+const Container = styled.div`
+  @media (max-width: 767px) {
+    padding-bottom: 20px;
+    margin-top: 20px;
+  }
+
+  @media (min-width: 768px) {
+    display: flex;
+    width: 100%;
+    height: 90%;
+  }
 `;
 
 const MyPage = () => {
@@ -10,6 +24,8 @@ const MyPage = () => {
     <>
       <Header title="통계" />
       <Line style={{ height: '2px' }} />
+      <HeaderDate />
+      <Container></Container>
     </>
   );
 };
