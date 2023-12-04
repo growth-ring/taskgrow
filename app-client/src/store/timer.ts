35de4190238e3d08423fdff3570ab980dc677a34
JiHo Kim <kr36510@naver.com>
2023-12-05 00:32:00 +0900

    타이머 미실행시 다른 버튼 클릭시 Alert창이 뜨지 않도록 하라

--- app-client/src/store/timer.ts
+++ app-client/src/store/timer.ts
@@ -29,7 +29,7 @@ export const useTimerStore = create<TimerStore>((set) => ({
   showReview: () => set({ selectedBtn: 'REVIEW' }),
   onTimer: userStartTime === 0 && userTodo !== '휴식' ? false : true,
   setOnTimer: (timer) => set({ onTimer: timer }),
-  timerState: 'INITIAL',
+  timerState: userStartTime ? 'RUNNING' : 'INITIAL',
   startTime: userStartTime,
   start: () => {
     const userStartTime = Date.now();
