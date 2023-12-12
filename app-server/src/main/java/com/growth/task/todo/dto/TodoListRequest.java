b2b874d3f614d06cf7ba0bb9021d9216c8963ef1
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-12-12 13:33:03 +0900

    Status를 받도록 수정하라

--- app-server/src/main/java/com/growth/task/todo/dto/TodoListRequest.java
+++ app-server/src/main/java/com/growth/task/todo/dto/TodoListRequest.java
@@ -3,14 +3,13 @@ package com.growth.task.todo.dto;
 import com.growth.task.todo.enums.Status;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
 
-@ToString
 @Getter
 public class TodoListRequest {
     private Status status;
+
     @Builder
-    public TodoListRequest(String status) {
-        this.status = Status.valueOf(status);
+    public TodoListRequest(Status status) {
+        this.status = status;
     }
 }
