4f29d98b021c5958ee003b5e9374ae8fdb0e2d0a
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-12-12 13:34:15 +0900

    userId와 기분점수, 시간 범위에 해다하는 회고 리스트를 조회하는 쿼리 메소드를 구현하라

--- app-server/src/main/java/com/growth/task/review/repository/impl/ReviewRepositoryCustomImpl.java
+++ app-server/src/main/java/com/growth/task/review/repository/impl/ReviewRepositoryCustomImpl.java
@@ -1,11 +1,16 @@
 package com.growth.task.review.repository.impl;
 
 import com.growth.task.review.dto.ReviewDetailResponse;
+import com.growth.task.review.dto.ReviewDetailWithTaskDateResponse;
 import com.growth.task.review.dto.ReviewStatsRequest;
 import com.growth.task.review.repository.ReviewRepositoryCustom;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -31,26 +36,77 @@ public class ReviewRepositoryCustomImpl implements ReviewRepositoryCustom {
                         review.feelingsScore
                 ))
                 .from(review)
-                .leftJoin(tasks)
+                .innerJoin(tasks)
                 .on(review.tasks.eq(tasks))
                 .where(
                         eqUserId(userId),
-                        betweenTimeRange(request)
+                        betweenTimeRange(request.getStartDate(), request.getEndDate())
                 )
                 .fetch()
                 ;
     }
 
-    private static BooleanExpression eqUserId(Long userId) {
+    @Override
+    public Page<ReviewDetailWithTaskDateResponse> findByUserAndParams(
+            Pageable pageable,
+            Long userId,
+            Integer feelingsScore,
+            LocalDate startDate,
+            LocalDate endDate
+    ) {
+        List<ReviewDetailWithTaskDateResponse> content = queryFactory
+                .select(Projections.fields(ReviewDetailWithTaskDateResponse.class,
+                        review.id.as("reviewId"),
+                        review.subject,
+                        review.contents,
+                        review.feelingsScore,
+                        tasks.taskDate
+                ))
+                .from(review)
+                .innerJoin(tasks)
+                .on(review.tasks.eq(tasks))
+                .where(
+                        eqUserId(userId),
+                        eqFeelingsScore(feelingsScore),
+                        betweenTimeRange(startDate, endDate)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(tasks.taskDate.desc())
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(review.count())
+                .from(review)
+                .innerJoin(tasks)
+                .on(review.tasks.eq(tasks))
+                .where(
+                        eqUserId(userId),
+                        eqFeelingsScore(feelingsScore),
+                        betweenTimeRange(startDate, endDate)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize());
+
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+    }
+
+    private BooleanExpression eqUserId(Long userId) {
         return tasks.user.userId.eq(userId);
     }
 
-    private static BooleanExpression betweenTimeRange(ReviewStatsRequest request) {
-        LocalDate startDate = request.getStartDate();
-        LocalDate endDate = request.getEndDate();
+    private BooleanExpression betweenTimeRange(LocalDate startDate, LocalDate endDate) {
         if (startDate == null && endDate == null) {
             return null;
         }
         return tasks.taskDate.between(startDate, endDate);
+    }
+
+    private BooleanExpression eqFeelingsScore(Integer feelingsScore) {
+        System.out.println("repository feelingsScore: " + feelingsScore);
+        if (feelingsScore == null) {
+            return null;
+        }
+        return review.feelingsScore.eq(feelingsScore);
     }
 }
