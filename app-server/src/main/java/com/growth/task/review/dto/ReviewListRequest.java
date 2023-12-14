fea6596eb1af7e4a6d3e5524ce28e17ee5bd3db2
leekyuwon <kyuwoon369@gmail.com>
2023-12-14 17:27:51 +0900

    기분점수 파라미터를 여러개를 받아 리스트를 조회하라

--- app-server/src/main/java/com/growth/task/review/dto/ReviewListRequest.java
+++ app-server/src/main/java/com/growth/task/review/dto/ReviewListRequest.java
@@ -2,22 +2,18 @@ package com.growth.task.review.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Getter
 public class ReviewListRequest {
     private static final String FEELINGS_SCORE_VALID_MESSAGE = "기분 점수는 1 ~ 10 사이를 입력해주세요.";
-
-    @Min(value = 1, message = FEELINGS_SCORE_VALID_MESSAGE)
-    @Max(value = 10, message = FEELINGS_SCORE_VALID_MESSAGE)
-    private Integer feelingsScore;
+    private List<Integer> feelingsScore;
     @DateTimeFormat(pattern = "YYYY-MM-dd")
     private LocalDate startDate;
     @DateTimeFormat(pattern = "YYYY-MM-dd")
@@ -25,7 +21,7 @@ public class ReviewListRequest {
 
     @Builder
     public ReviewListRequest(
-            Integer feelings_score,
+            List<Integer> feelings_score,
             LocalDate start_date,
             LocalDate end_date
     ) {
