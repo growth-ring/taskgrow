bc0828d269c576432d40a9a56ad9e01ec95a28c9
leekyuwon <kyuwoon369@gmail.com>
2023-12-20 23:13:11 +0900

    Json 응답시 스네이크 표기법으로 파싱하라

--- app-server/src/main/java/com/growth/task/review/dto/ReviewStatsResponse.java
+++ app-server/src/main/java/com/growth/task/review/dto/ReviewStatsResponse.java
@@ -1,10 +1,13 @@
 package com.growth.task.review.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @NoArgsConstructor
 @Getter
 public class ReviewStatsResponse {
