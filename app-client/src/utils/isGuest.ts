c11bd8c8fb86b6c89d3c9053a471fd30913333eb
Jiho Kim <116357790+KJeeu@users.noreply.github.com>
2024-01-30 19:59:04 +0900

    guest 접속 안되는 현상 수정하라 (#279)

--- app-client/src/utils/isGuest.ts
+++ app-client/src/utils/isGuest.ts
@@ -1,7 +1,12 @@
+import moment from 'moment';
+
 export function isGuest() {
   const userNameId = localStorage.getItem('userNameId');
 
-  if (userNameId === '') {
+  const taskDate = moment(new Date()).format('YYYY-MM-DD');
+  localStorage.setItem('taskDate', taskDate);
+
+  if (userNameId === null || userNameId === '') {
     return true;
   } else {
     return false;
