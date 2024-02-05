c91de41bd18a9fdaff260d4497247174510b4298
Jiho Kim <116357790+KJeeu@users.noreply.github.com>
2024-02-05 15:18:02 +0900

    favicon 경로 변경하라 (#289)

--- app-client/src/hooks/useFavicon.ts
+++ app-client/src/hooks/useFavicon.ts
@@ -1,11 +1,11 @@
 const useFavicon = (percent: number, timerState: string) => {
   const selectClock = (percent: number) => {
     if (timerState !== 'RUNNING') {
-      return '../public/favicon.ico';
+      return '/favicon.ico';
     } else {
       const totalImages = 12;
       const imageIndex = Math.ceil((percent / 100) * totalImages);
-      return `../public/timer/timer${imageIndex}.ico`;
+      return `/timer/timer${imageIndex}.ico`;
     }
   };
 
