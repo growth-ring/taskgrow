938b20c96df375032de2f2f040ee477495988f0e
JiHo Kim <kr36510@naver.com>
2024-02-07 14:14:30 +0900

    tailwind에 datkGray색상 추가

--- app-client/tailwind.config.js
+++ app-client/tailwind.config.js
@@ -9,9 +9,11 @@ export default {
     colors: {
       'white': '#ffffff',
       'gray': '#5c5c5c',
+      'darkGray': 'rgb(200, 200, 200)',
       'lightGray': 'rgb(220, 220, 220)',
       'main-color': '#73b2fa',
     },
   },
   plugins: [],
 }
+
