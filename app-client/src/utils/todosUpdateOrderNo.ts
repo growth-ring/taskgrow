3f27945eecd681e86ba992f0cec8d2c38f2466c4
JiHo Kim <kr36510@naver.com>
2024-02-13 15:56:04 +0900

    todo type에 orderNo 추가하고 중복 제외하라

--- app-client/src/utils/todosUpdateOrderNo.ts
+++ app-client/src/utils/todosUpdateOrderNo.ts
@@ -1,22 +1,14 @@
 import { getTodos } from '../services/todo';
 import { updateTodoOrder } from '../services/todo';
 
-interface TodoData {
-  todoId: number;
-  taskId: number;
-  todo: string;
-  status: string;
-  performCount: number;
-  planCount: number;
-  orderNo: number;
-}
+import type { Todo } from '../store/todos';
 
 export const todosUpdateOrderNo = async (
   selectedTaskId: number,
   todoId: number,
 ) => {
   // 해당 task의 모든 todo를 불러옴
-  const todos: TodoData[] = await getTodos(selectedTaskId);
+  const todos: Todo[] = await getTodos(selectedTaskId);
 
   // 현재 todo의 orderNo을 알기
   const deleteTodo = todos.filter((todo) => {
