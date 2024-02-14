c6913488180e076606cba6f77a5a9d81c751e391
JiHo Kim <kr36510@naver.com>
2024-02-14 10:05:18 +0900

    드래그해서 할 일의 순서 변경시 다른 할 일의 orderNo 변동 발생

--- app-client/src/utils/todosUpdateOrderNo.ts
+++ app-client/src/utils/todosUpdateOrderNo.ts
@@ -2,7 +2,6 @@ import { getTodos } from '../services/todo';
 import { updateTodoOrder } from '../services/todo';
 
 import type { Todo } from '../store/todos';
-
 export const todosUpdateOrderNo = async (
   selectedTaskId: number,
   todoId: number,
@@ -23,4 +22,35 @@ export const todosUpdateOrderNo = async (
       updateTodoOrder(todo.todoId, todo.orderNo - 1);
     }
   });
+};
+
+//옮긴 투두의 order 뒤 부터 옮겨진 투두 order까지
+export const todosDragUpdateOrderNo = async (
+  selectedTaskId: number,
+  dragTodoOrderNo: number,
+  dragOverTodoOrderNo: number,
+) => {
+  // 해당 task의 모든 todo를 불러옴
+  const todos: Todo[] = await getTodos(selectedTaskId);
+
+  // dragTodo의 orderNo 뒤부터 ~ dragOverTodo의 orderNo 전까지
+  if (dragTodoOrderNo < dragOverTodoOrderNo) {
+    todos.forEach((todo) => {
+      if (
+        todo.orderNo > dragTodoOrderNo &&
+        todo.orderNo <= dragOverTodoOrderNo
+      ) {
+        updateTodoOrder(todo.todoId, todo.orderNo - 1);
+      }
+    });
+  } else {
+    todos.forEach((todo) => {
+      if (
+        todo.orderNo < dragTodoOrderNo &&
+        todo.orderNo >= dragOverTodoOrderNo
+      ) {
+        updateTodoOrder(todo.todoId, todo.orderNo + 1);
+      }
+    });
+  }
 };
