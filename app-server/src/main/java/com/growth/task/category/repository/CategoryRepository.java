87664aecda3258335ba35b3ea8e369fd13ddd9f6
leekyuwon <kyuwoon369@gmail.com>
2024-02-20 20:45:19 +0900

    카테고리명으로 카테고리를 조회하라

--- app-server/src/main/java/com/growth/task/category/repository/CategoryRepository.java
+++ app-server/src/main/java/com/growth/task/category/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.growth.task.category.repository;
 import com.growth.task.category.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 }
