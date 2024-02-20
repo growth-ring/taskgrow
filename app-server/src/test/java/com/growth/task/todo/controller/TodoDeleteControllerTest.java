3914e7f2aaccde8c24dde46c9e537433d44bb68a
leekyuwon <kyuwon.lee@makinarocks.ai>
2024-02-20 10:21:18 +0900

    주어진 투두의 카테고리를 제거하는 API를 구현하고 테스트하라

--- app-server/src/test/java/com/growth/task/todo/controller/TodoDeleteControllerTest.java
+++ app-server/src/test/java/com/growth/task/todo/controller/TodoDeleteControllerTest.java
@@ -1,12 +1,16 @@
 package com.growth.task.todo.controller;
 
+import com.growth.task.category.domain.Category;
+import com.growth.task.category.repository.CategoryRepository;
 import com.growth.task.pomodoro.domain.Pomodoros;
 import com.growth.task.pomodoro.repository.PomodorosRepository;
 import com.growth.task.task.domain.Tasks;
 import com.growth.task.task.repository.TasksRepository;
 import com.growth.task.todo.application.TodoDeleteService;
+import com.growth.task.todo.domain.TodoCategory;
 import com.growth.task.todo.domain.Todos;
 import com.growth.task.todo.enums.Status;
+import com.growth.task.todo.repository.TodoCategoryRepository;
 import com.growth.task.todo.repository.TodosRepository;
 import com.growth.task.user.domain.Users;
 import com.growth.task.user.domain.UsersRepository;
@@ -53,17 +57,16 @@ class TodoDeleteControllerTest {
     @Autowired
     private PomodorosRepository pomodorosRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private TodoCategoryRepository todoCategoryRepository;
+
     @BeforeEach
     void setUp() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(wac)
                 .alwaysDo(print())
                 .build();
-    }
-
-    private ResultActions performDelete(Long todoId) throws Exception {
-        return mockMvc.perform(delete("/api/v1/todos/{todo_id}", todoId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON));
     }
 
     private Users getUser(String name, String password) {
@@ -105,6 +108,14 @@ class TodoDeleteControllerTest {
         );
     }
 
+    private Category createCategory(String name) {
+        return categoryRepository.save(Category.builder().name(name).build());
+    }
+
+    private TodoCategory addTodoCategory(Todos todo, Category category) {
+        return todoCategoryRepository.save(new TodoCategory(todo, category));
+    }
+
     @AfterEach
     void cleanUp() {
         pomodorosRepository.deleteAll();
@@ -116,6 +127,12 @@ class TodoDeleteControllerTest {
     @Nested
     @DisplayName("DELETE /api/v1/todos/{todo_id} 요청은")
     class Describe_delete {
+        private ResultActions performDelete(Long todoId) throws Exception {
+            return mockMvc.perform(delete("/api/v1/todos/{todo_id}", todoId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON));
+        }
+
         private Long todoId;
         private Users user;
         private Tasks task;
@@ -144,6 +161,49 @@ class TodoDeleteControllerTest {
                 performDelete(todoId)
                         .andExpect(status().isNoContent());
                 assertNull(pomodorosRepository.findById(pomodoros.getPomodoroId()).orElse(null));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("DELETE /api/v1/todos/{todo_id}/category 요청은")
+    class Describe_delete_category {
+        private Long todoId;
+        private Users user;
+        private Tasks task;
+        private Todos todo;
+        private Pomodoros pomodoros;
+
+        private ResultActions subject(Long todoId) throws Exception {
+            return mockMvc.perform(delete("/api/v1/todos/{todo_id}/category", todoId))
+                    ;
+        }
+
+        @BeforeEach
+        void setUp() {
+            user = getUser("user", "password");
+            task = getTask(user, LocalDate.parse("2023-09-26"));
+            todo = getTodo(task, "코딩 테스트", Status.READY);
+            pomodoros = getPomodoros(todo, 0, 3);
+            Category category = createCategory("공부");
+            addTodoCategory(todo, category);
+        }
+
+        @Nested
+        @DisplayName("유효한 todo_id 가 존재하는 경우")
+        class Context_exists_valid_todoId {
+            @BeforeEach
+            void setUp() {
+                todoId = todo.getTodoId();
+            }
+
+            @Test
+            @DisplayName("204 상태 코드와 함께 todo의 카테고리가 삭제된다.")
+            void it_response_no_content_and_delete_category() throws Exception {
+                final ResultActions resultActions = subject(todoId);
+                resultActions.andExpect(status().isNoContent());
+
+                assertNull(todoCategoryRepository.findByTodos(todo).orElse(null));
             }
         }
     }
