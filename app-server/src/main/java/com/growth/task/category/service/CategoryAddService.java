6b5edfd8afb03137d526f475bca1fe5a234c6a33
leekyuwon <kyuwoon369@gmail.com>
2024-02-20 20:46:14 +0900

    카테고리 추가시 카테고리명으로 검색하여 중복의 경우 예외를 던지도록 하라

--- app-server/src/main/java/com/growth/task/category/service/CategoryAddService.java
+++ app-server/src/main/java/com/growth/task/category/service/CategoryAddService.java
@@ -3,6 +3,7 @@ package com.growth.task.category.service;
 import com.growth.task.category.domain.Category;
 import com.growth.task.category.dto.CategoryRequest;
 import com.growth.task.category.dto.CategoryResponse;
+import com.growth.task.category.exception.CategoryAlreadyExistsException;
 import com.growth.task.category.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,11 @@ public class CategoryAddService {
 
     @Transactional
     public CategoryResponse save(CategoryRequest request) {
+        categoryRepository.findByName(request.getName())
+                .ifPresent(it -> {
+                    throw new CategoryAlreadyExistsException();
+                });
+
         Category category = categoryRepository.save(request.toEntity());
         return CategoryResponse.of(category);
     }
